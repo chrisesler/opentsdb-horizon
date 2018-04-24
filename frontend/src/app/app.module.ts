209cfc934e3f93381fb6ec11e3656ab4e4b7d72e
Hill Nguyen <hilln@oath.com>
2018-04-24 16:10:43 -0700

    add adhoc, admin, dashboard, header modules and d3

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
+
+// custom modules
+import { HeaderModule } from './header/header.module';
+import { AdminModule } from './admin/admin.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AdhocModule } from './adhoc/adhoc.module';
+
+// custom services
+import { D3Service } from './shared/services/d3.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +20,13 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    HeaderModule,
+    AdminModule,
+    DashboardModule,
+    AdhocModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
