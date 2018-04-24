ab7e58b6c83b56d10c4ff401a676b0b430f82973
Hill Nguyen <hilln@oath.com>
2018-04-24 16:43:05 -0700

    rm header modules, add navbar to shared

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/modules/material/material.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 // custom modules
-import { HeaderModule } from './header/header.module';
 import { AdminModule } from './admin/admin.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { AdhocModule } from './adhoc/adhoc.module';
@@ -14,15 +13,20 @@ import { AdhocModule } from './adhoc/adhoc.module';
 // custom services
 import { D3Service } from './shared/services/d3.service';
 
+// component
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    HeaderModule,
     AdminModule,
     DashboardModule,
     AdhocModule
