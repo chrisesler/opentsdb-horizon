64a5b26ec228aba7f01213173fc167d899761b63
Hill Nguyen <hilln@oath.com>
2018-04-25 14:22:15 -0700

    create utils service and using on CoreModule

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent} from './components/navbar/navbar.component';
 import { D3Service } from './services/d3.service';
+import { UtilsService } from './services/utils.service';
+
 @NgModule({
   imports: [
     CommonModule
@@ -14,10 +16,11 @@ import { D3Service } from './services/d3.service';
     NavbarComponent
   ],
   providers: [
-    D3Service
+    D3Service,
+    UtilsService
   ]
 })
-export class CoreModule { 
+export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
       throw new Error(`CoreModule has already been loaded. Import Core modules in the AppModule only.`);
