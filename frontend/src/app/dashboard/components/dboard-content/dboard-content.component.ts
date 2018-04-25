cb40a2420bc75b84513c2bfb726cb88d8fa2d7c6
Hill Nguyen <hilln@oath.com>
2018-04-25 14:15:03 -0700

    implement dashboard module and components

---
+++ frontend/src/app/dashboard/components/dboard-content/dboard-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dboard-content',
+  templateUrl: './dboard-content.component.html',
+  styleUrls: ['./dboard-content.component.scss']
+})
+export class DboardContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/dashboard/components/dboard-header/dboard-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dboard-header',
+  templateUrl: './dboard-header.component.html',
+  styleUrls: ['./dboard-header.component.scss']
+})
+export class DboardHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
