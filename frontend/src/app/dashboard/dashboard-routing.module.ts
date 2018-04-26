e42457694102b551391151f3f3472190bea650bd
Hill Nguyen <hilln@oath.com>
2018-04-25 17:48:44 -0700

    update routing and dashboard module with all new directives and services

--- frontend/src/app/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+const routes: Routes = [
+  { path: '', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
