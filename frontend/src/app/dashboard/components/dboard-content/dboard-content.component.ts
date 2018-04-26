b24d87c7aaaa1344ce4b22cc77fb099642bd1a2a
Hill Nguyen <hilln@oath.com>
2018-04-25 17:50:50 -0700

    componet to hold dashboard main content

--- frontend/src/app/dashboard/components/dboard-content/dboard-content.component.ts
+++ frontend/src/app/dashboard/components/dboard-content/dboard-content.component.ts
@@ -1,15 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, Input, ViewChild, ViewEncapsulation,
+  ChangeDetectionStrategy, OnChanges, SimpleChanges, ComponentFactoryResolver
+} from '@angular/core';
+import { GridsterComponent, GridsterItemComponent, IGridsterOptions, IGridsterDraggableOptions } from 'angular2gridster';
+import { WidgetViewDirective } from '../../directives/widgetview.directive';
+import { WidgetComponent } from '../../widgets/widgetcomponent';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-dboard-content',
   templateUrl: './dboard-content.component.html',
-  styleUrls: ['./dboard-content.component.scss']
+  styleUrls: ['./dboard-content.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class DboardContentComponent implements OnInit {
+export class DboardContentComponent implements OnInit, OnChanges {
+  @ViewChild(WidgetViewDirective) widgetViewContainer: WidgetViewDirective;
+  @ViewChild(GridsterComponent) gridster: GridsterComponent;
+  @Input() widgets: any[];
+  @Input() rerender: any;
 
-  constructor() { }
+  viewEditMode = false;
+  cellHeight = 0;
+  cellWidth = 0;
 
-  ngOnInit() {
+  constructor(private dbService: DashboardService) { }
+
+  gridsterOptions: IGridsterOptions = {
+    // core configuration is default one - for smallest view. It has hidden minWidth: 0.
+    lanes: 1, // amount of lanes (cells) in the grid
+    direction: 'vertical', // floating top - vertical, left - horizontal
+    floating: true, // no gravity floating
+    dragAndDrop: true, // enable/disable drag and drop for all items in grid
+    resizable: true, // enable/disable resizing by drag and drop for all items in grid
+    resizeHandles: {
+      s: true,
+      e: true,
+      se: true
+    },
+    widthHeightRatio: 2, // proportion between item width and height
+    lines: {
+      visible: false,
+      color: '#afafaf',
+      width: 1
+    },
+    shrink: true,
+    useCSSTransforms: true,
+    responsiveView: true, // turn on adopting items sizes on window resize and enable responsiveOptions
+    responsiveDebounce: 200, // window resize debounce time
+    // List of different gridster configurations for different breakpoints.
+    // Each breakpoint is defined by name stored in "breakpoint" property. There is fixed set of breakpoints
+    // available to use with default minWidth assign to each.
+    // - sm: 576 - Small devices
+    // - md: 768 - Medium devices
+    // - lg: 992 - Large devices
+    // - xl: 1200 - Extra large
+    // MinWidth for each breakpoint can be overwritten like it's visible below.
+    // ResponsiveOptions can overwrite default configuration with any option available.
+    responsiveOptions: [
+      {
+        breakpoint: 'sm',
+        direction: 'vertical',
+        lanes: 1,
+      },
+      {
+        breakpoint: 'md',
+        minWidth: 768,
+        direction: 'verical',
+        lanes: 12,
+        resizable: true
+      }
+    ]
+  };
+
+  gridsterDraggableOptions: IGridsterDraggableOptions = {
+    handlerClass: 'panel-heading'
+  };
+
+  ngOnInit() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('sasas', changes);
+    if (changes.rerender && changes.rerender.currentValue.reload) {
+      this.gridster.reload();
+    }
+  }
+
+  closeViewEdit() {
+    this.widgetViewContainer.viewContainerRef.clear();
+    this.viewEditMode = false;
+  }
+  // to load selected component factory
+  viewComponent(comp: any) {
+    this.viewEditMode = true;
+    const viewContainerRef = this.widgetViewContainer.viewContainerRef;
+    viewContainerRef.clear();
+    const component = viewContainerRef.createComponent(comp.compFactory);
+    (<WidgetComponent>component.instance).config = comp.config;
+  }
+
+  breakpointChange(event: IGridsterOptions) {
+    console.log('breakpoint', event);
+    let ratio = 2;
+    if (event.lanes === 1) {
+      ratio = 4;
+    }
+    if (this.gridster.isReady) {
+      this.gridster.setOption('widthHeightRatio', ratio).reload();
+    }
+  }
+  // this event will start first and set values of cellWidth and cellHeight
+  // then update the this.widgets reference
+  gridsterFlow(event: any) {
+    console.log('reflow', event, event.gridsterComponent.gridster.cellHeight);
+    this.cellHeight = event.gridsterComponent.gridster.cellHeight;
+    this.cellWidth = event.gridsterComponent.gridster.cellWidth;
+    this.dbService.updateWidgetsDimension(this.cellWidth, this.cellHeight, this.widgets);
+    console.log('windows resize', this.widgets);
+  }
+
+  // this event happened when item is dragged or resize
+  // we call the function update all since we don't know which one for now.
+  gridEventEnd(event: any) {
+    // console.log(event, event.item.$element.getBoundingClientRect());
+    if (event.action === 'resize') {
+      this.dbService.updateWidgetsDimension(this.cellWidth, this.cellHeight, this.widgets);
+      console.log('item resize', this.widgets);
+    }
+
   }
 
 }
