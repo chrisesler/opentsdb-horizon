c63c036111d5af4b49764471d3f5e20341395db3
Hill Nguyen <hilln@oath.com>
2018-05-10 14:33:32 -0700

    mock dashboard data

--- frontend/src/app/core/http/http.service.ts
+++ frontend/src/app/core/http/http.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class HttpService {
 
   constructor(private http: HttpClient) { }
 
+  private dashboard: any  = {
+    id: '123456',
+    settings: {
+      title: 'my dashboard'
+    },
+    widgets: [
+      {
+        gridPos: {
+          x: 0, y: 0,
+          w: 6, h: 5
+        },
+        config: {
+          title: 'One',
+          component_type: 'WsampleComponent'
+        }
+      },
+      {
+        gridPos: {
+          x: 0, y: 5,
+          w: 6, h: 5,
+        },
+        config: {
+          title: 'Two',
+          component_type: 'WsampleComponent'
+        }
+      },
+      {
+        gridPos: {
+          x: 6, y: 0,
+          w: 6, h: 5
+        },
+        config: {
+          title: 'Three'
+        }
+      },
+      {
+        gridPos: {
+          x: 6, y: 5,
+          w: 6, h: 5,
+        },
+        config: {
+          title: 'Four',
+          component_type: 'StaticImageComponent'
+        }
+      }
+    ]
+  };
+
+  getDashoard(id: string): Observable<any> {
+    return Observable.of(this.dashboard);
+  }
 }
