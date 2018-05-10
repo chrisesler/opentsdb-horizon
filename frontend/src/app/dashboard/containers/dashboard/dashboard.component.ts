c641c3f91e44a4c65d8ad277dff45f392f9e055e
Hill Nguyen <hilln@oath.com>
2018-05-10 14:34:26 -0700

    update smart component with selector and store dispatch

--- frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
@@ -1,35 +1,45 @@
-import { Component, OnInit, OnDestroy, HostBinding  } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding, ChangeDetectionStrategy  } from '@angular/core';
 import { DashboardService } from '../../services/dashboard.service';
 import { IntercomService, IMessage } from '../../services/intercom.service';
 import { Subscription } from 'rxjs/Subscription';
-import { Store } from '@ngxs/store';
+import { Store, Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { LoadDashboard } from '../../state/dashboard.actions';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
   @HostBinding('class.app-dashboard') private hostClass = true;
 
+  // @Select((state: any) => state.dashboardState.widgets)
+  // widgets$: Observable<any[]>;
+
+
   listenSub: Subscription;
-  dashboard: any;
-  // widgets: Array<any>;
+  widgets$: Observable<any>;
   hasHeader: boolean;
   rerender: any = {'reload': false};
-  constructor(private store: Store, private interCom: IntercomService, private dbService: DashboardService) { }
+  constructor(private store: Store, private interCom: IntercomService, private dbService: DashboardService) { 
+    this.widgets$ = this.store.select(state => state.dashboardState.widgets);
+  }
 
   ngOnInit() {
+    console.log('snapshot', this.store.snapshot());
+    
       this.hasHeader = true;
-      this.dashboard = this.dbService.dashboard;
+      //this.dashboard = this.dbService.dashboard;
       // console.log('dash', this.dashboard);
       // this.widgets = this.dashboard.widgets;
       // ready to handle request from children of DashboardModule
       this.listenSub = this.interCom.requestListen().subscribe((message: IMessage) => {
         // console.log('listen to: ', JSON.stringify(message));
       });
-      console.log('store', this.store.snapshot());
+      this.store.dispatch(new LoadDashboard('xyz'));
       
    }
 
