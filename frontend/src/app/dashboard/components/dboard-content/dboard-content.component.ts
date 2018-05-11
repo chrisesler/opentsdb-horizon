4a11577048fca0537a2adb5d125a8c9595533147
Hill Nguyen <hilln@oath.com>
2018-05-11 15:26:16 -0700

    enable OnPush change detection on dashboard-content level

--- frontend/src/app/dashboard/components/dboard-content/dboard-content.component.ts
+++ frontend/src/app/dashboard/components/dboard-content/dboard-content.component.ts
@@ -1,20 +1,20 @@
 import {
   Component, OnInit, Input, ViewChild, ViewEncapsulation,
   ChangeDetectionStrategy, OnChanges, SimpleChanges, ComponentFactoryResolver,
-  HostBinding
+  HostBinding, Output, EventEmitter
 } from '@angular/core';
 import { GridsterComponent, GridsterItemComponent, IGridsterOptions, IGridsterDraggableOptions } from 'angular2gridster';
 import { WidgetViewDirective } from '../../directives/widgetview.directive';
 import { WidgetComponent } from '../../widgets/widgetcomponent';
 import { DashboardService } from '../../services/dashboard.service';
-import { Store } from '@ngxs/store';
 import { IntercomService, IMessage } from '../../services/intercom.service';
 
 @Component({
   selector: 'app-dboard-content',
   templateUrl: './dboard-content.component.html',
   styleUrls: ['./dboard-content.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DboardContentComponent implements OnInit, OnChanges {
   @HostBinding('class.app-dboard-content') private _hostClass = true;
@@ -23,6 +23,7 @@ export class DboardContentComponent implements OnInit, OnChanges {
   @ViewChild(WidgetViewDirective) widgetViewContainer: WidgetViewDirective;
   @ViewChild(GridsterComponent) gridster: GridsterComponent;
 
+  @Output() widgetsLayoutUpdate = new EventEmitter();
   @Input() widgets: any[];
   @Input() rerender: any;
 
@@ -41,7 +42,7 @@ export class DboardContentComponent implements OnInit, OnChanges {
   cellHeight = 0;
   cellWidth = 0;
 
-  constructor(private dbService: DashboardService, private interCom: IntercomService, private store: Store) { }
+  constructor(private dbService: DashboardService, private interCom: IntercomService) { }
 
   gridsterOptions: IGridsterOptions = {
     // core configuration is default one - for smallest view. It has hidden minWidth: 0.
@@ -85,10 +86,12 @@ export class DboardContentComponent implements OnInit, OnChanges {
   ngOnInit() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log('sasas', changes);
+    console.log('dbaord-content-changes', changes);
+    
+    // need to reload grister view to update the UI
     if (changes.rerender && changes.rerender.currentValue.reload) {
-      this.gridster.reload();
-    }
+       this.gridster.reload();
+    } 
   }
 
   closeEditView() {
@@ -98,6 +101,8 @@ export class DboardContentComponent implements OnInit, OnChanges {
 
   // to load selected component factory
   viewComponent(comp: any) {
+    console.log('view component', comp);
+ 
     // get the view container
     const viewContainerRef = this.widgetViewContainer.viewContainerRef;
     viewContainerRef.clear();
@@ -105,13 +110,12 @@ export class DboardContentComponent implements OnInit, OnChanges {
     // create component using existing widget factory
     const component = viewContainerRef.createComponent(comp.compFactory);
 
-    // give the widget it's config
-    (<WidgetComponent>component.instance).config = comp.config;
-
+    // assign @input widget
+    (<WidgetComponent>component.instance).widget = comp.widget; 
   }
 
+  // change ratio when breakpoint hits
   breakpointChange(event: IGridsterOptions) {
-    console.log('breakpoint', event);
     let ratio = 2;
     if (event.lanes === 1) {
       ratio = 4;
@@ -120,6 +124,7 @@ export class DboardContentComponent implements OnInit, OnChanges {
       this.gridster.setOption('widthHeightRatio', ratio).reload();
     }
   }
+
   // this event will start first and set values of cellWidth and cellHeight
   // then update the this.widgets reference
   gridsterFlow(event: any) {
@@ -127,7 +132,8 @@ export class DboardContentComponent implements OnInit, OnChanges {
     this.cellHeight = event.gridsterComponent.gridster.cellHeight;
     this.cellWidth = event.gridsterComponent.gridster.cellWidth;
     this.dbService.updateWidgetsDimension(this.cellWidth, this.cellHeight, this.widgets);
-    console.log('state', this.store.snapshot());
+    // console.log('current widget', this.widgets);
+    this.widgetsLayoutUpdate.emit(this.widgets);
     
   }
 
@@ -138,9 +144,9 @@ export class DboardContentComponent implements OnInit, OnChanges {
     //console.log(event, event.item.$element.getBoundingClientRect());
     if (event.action === 'resize') {
       this.dbService.updateWidgetsDimension(this.cellWidth, this.cellHeight, this.widgets);
+      this.widgetsLayoutUpdate.emit(this.widgets);
       //console.log('item resize', this.widgets);
     }
-    console.log('state', this.store.snapshot());
   }
 
 }
