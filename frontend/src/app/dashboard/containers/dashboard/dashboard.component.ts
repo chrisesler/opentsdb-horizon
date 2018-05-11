f61dbd5adbfa640a423a1514afce3f645d8cc8ba
Hill Nguyen <hilln@oath.com>
2018-05-11 15:24:59 -0700

    implement UpdateWidgewtLayout function when window or item resizes

--- frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
@@ -7,8 +7,9 @@ import { DashboardService } from '../../services/dashboard.service';
 import { IntercomService, IMessage } from '../../services/intercom.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Store, Select } from '@ngxs/store';
+import { DashboardState } from '../../state/dashboard.state';
 import { Observable } from 'rxjs';
-import { LoadDashboard } from '../../state/dashboard.actions';
+import * as dashboardActions from '../../state/dashboard.actions';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,31 +20,25 @@ import { LoadDashboard } from '../../state/dashboard.actions';
 export class DashboardComponent implements OnInit, OnDestroy {
 
   @HostBinding('class.app-dashboard') private hostClass = true;
-
-  // @Select((state: any) => state.dashboardState.widgets)
-  // widgets$: Observable<any[]>;
   @ViewChild('testTmpl') testTmpl: TemplateRef<any>;
 
+  //@Select(DashboardState.getWidgets) widgets$: Observable<any>;
+  @Select(state => state.dashboardState.widgets) widgets$: Observable<any>;
+
   listenSub: Subscription;
-  widgets$: Observable<any>;
   viewEditMode = false;
   rerender: any = {'reload': false};
+  // need this?
+  widgets: any[] = [];
   constructor(
       private store: Store, 
       private interCom: IntercomService, 
       private dbService: DashboardService,
       private cdkService: CdkService
-  ) { 
-    this.widgets$ = this.store.select(state => state.dashboardState.widgets);
-  }
+  ) {}
 
   ngOnInit() {
     console.log('snapshot', this.store.snapshot());
-    
-      // this.hasHeader = true;
-      // this.dashboard = this.dbService.dashboard;
-      // console.log('dash', this.dashboard);
-      // this.widgets = this.dashboard.widgets;
       // ready to handle request from children of DashboardModule
       this.listenSub = this.interCom.requestListen().subscribe((message: IMessage) => {
         console.log('listen to: ', JSON.stringify(message));
@@ -56,13 +51,29 @@ export class DashboardComponent implements OnInit, OnDestroy {
           this.addNewWidget();
         }
       });
-      this.store.dispatch(new LoadDashboard('xyz'));
+      this.store.dispatch(new dashboardActions.LoadDashboard('xyz'));
+      this.widgets$.subscribe(widgets => { 
+        this.widgets = widgets;       
+      });
       this.cdkService.setNavbarPortal(new TemplatePortal(this.testTmpl, undefined, {}));
 
    }
 
+   // this will call based on gridster reflow and size changes
+   widgetsLayoutUpdate(widgets: any[]) {
+     this.store.dispatch(new dashboardActions.UpdateWidgetsLayout({widgets}));
+     // we the broadcast new dimention down to them for resizing
+     for (let i = 0; i < widgets.length; i++) {
+      this.interCom.responsePut({ 
+        id: widgets[i].id,
+        action: "resizeWidget",
+        payload: widgets[i].clientSize}
+      );
+     }
+   }
+
    addNewWidget() {
-     this.dbService.addNewWidget();
+     this.widgets = this.dbService.addNewWidget(this.widgets);
      this.rerender = {'reload': true};
    }
 
@@ -77,9 +88,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
    cdkTest() {
      console.log('CDK TEST');
      this.cdkService.setNavbarPortal(null);
-     //this.cdkService.setNavbarPortal(new TemplatePortal(this.testTmpl, undefined, {}));
-
-     
+     //this.cdkService.setNavbarPortal(new TemplatePortal(this.testTmpl, undefined, {}));  
    }
 
 }
