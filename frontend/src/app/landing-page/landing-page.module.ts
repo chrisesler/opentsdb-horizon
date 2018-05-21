986ca877c69142547e3f6f77eb93015a00446ed1
Chris Esler <cesler@oath.com>
2018-05-21 12:58:04 -0700

    Start of landing page. Nothing yet

---
+++ frontend/src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ],
+  declarations: []
+})
+export class LandingPageModule { }
