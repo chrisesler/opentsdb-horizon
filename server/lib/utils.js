5aca96f543c4780e882711d580a2884a6d977b7f
Hill Nguyen <hilln@oath.com>
2018-05-22 09:59:31 -0700

    some utilities for getting app config

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -14,6 +14,24 @@ if(fs.existsSync(path.resolve(__dirname, '../config/config.properties'))){
 
 self.getProperty = function (keyName) {
     return properties.get(keyName);
-}
+};
+
+self.getEnv = function() {
+    if (process.env.NODE_ENV === 'dev') {
+        return 'dev';
+    } else {
+        return 'prod';
+    }
+};
+
+self.getConfigByEnv = function (env) {
+    var config = require('../config/app_config');
+    return config[env];
+};
+
+self.getConfig = function() {
+    var env = this.getEnv();
+    return this.getConfigByEnv(env);
+};
 
 module.exports = self;
