934b93df95fd92bd7f89a13f6ce162ab00564070
Hill Nguyen <hilln@oath.com>
2018-06-18 11:09:07 -0700

    dygrahph option interface

---
+++ frontend/src/app/shared/modules/dygraphs/IDygraphOptions.ts
@@ -0,0 +1,8 @@
+// continue adding options
+export interface IDygraphOptions {
+    width?: number;
+    height?: number;
+    labels?: any;
+    connectSeparatedPoints?: boolean;
+    drawPoints?: boolean;
+}
