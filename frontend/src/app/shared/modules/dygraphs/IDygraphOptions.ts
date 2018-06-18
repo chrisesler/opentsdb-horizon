be859b138e98a5ef1bf70fb80b80f1b8ab3a631d
Hill Nguyen <hilln@oath.com>
2018-06-18 15:26:35 -0700

    add file and labelsDivWitdth attrs to dygrapg options model

--- frontend/src/app/shared/modules/dygraphs/IDygraphOptions.ts
+++ frontend/src/app/shared/modules/dygraphs/IDygraphOptions.ts
@@ -3,6 +3,8 @@ export interface IDygraphOptions {
     width?: number;
     height?: number;
     labels?: any;
+    labelsDivWidth?: number; // width for label legend
     connectSeparatedPoints?: boolean;
     drawPoints?: boolean;
+    file?: any;
 }
