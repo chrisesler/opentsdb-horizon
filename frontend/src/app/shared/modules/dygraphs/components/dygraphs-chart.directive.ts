4df9ef158db11d9a9c23770dd1a9908fe5976f7c
Hill Nguyen <hilln@oath.com>
2018-06-20 12:12:39 -0700

    add more options for dygraph and handle changes to render

--- frontend/src/app/shared/modules/dygraphs/components/dygraphs-chart.directive.ts
+++ frontend/src/app/shared/modules/dygraphs/components/dygraphs-chart.directive.ts
@@ -8,7 +8,7 @@ import Dygraph from 'dygraphs';
 })
 export class DygraphsChartDirective implements OnInit, OnChanges, OnDestroy {
 
-  @Input() data: any[];
+  @Input() data: any;
   @Input() options: IDygraphOptions;
   @Input() chartType: string;
   @Input() size: any;
@@ -30,22 +30,22 @@ export class DygraphsChartDirective implements OnInit, OnChanges, OnDestroy {
       console.log('no changes');
       return;
     } else {
-      console.log('changes', new Date().getMilliseconds(), changes);
+      // console.log('changes', new Date().getMilliseconds(), changes);
       // if not then create it
       if(!this._g && this.data) {
-        console.log('create dygraph object');
+        // console.log('create dygraph object');
         this._g = new Dygraph(this.element.nativeElement, this.data, this.options);
       }
       // resize when size be changed
-      if(this._g && changes.size.currentValue) {
-        console.log('call resize'); 
+      if(this._g && changes.size && changes.size.currentValue) {
+        console.log('call resize', changes.size.currentValue); 
         let nsize = changes.size.currentValue;    
         this._g.resize(nsize.width - 24, nsize.height - 50);
       }
 
       // if new data
       if (this._g && changes.data && changes.data.currentValue) {
-        console.log(' call new data', changes.data.currentValue);
+        // console.log(' call new data', changes.data.currentValue);
         let ndata = changes.data.currentValue;
         this.options = {...this.options, file: ndata}
         this._g.updateOptions(this.options);
