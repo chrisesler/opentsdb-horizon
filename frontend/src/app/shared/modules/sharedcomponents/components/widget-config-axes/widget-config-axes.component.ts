bd609b02ebd7d3ca9ae1fc9fbf479c65a2638b82
Chris Esler <cesler@oath.com>
2018-06-27 14:16:35 -0700

    Working on the various configs. Also updating the other placeholder components to reflect the config components. Some minor theme updates.

--- frontend/src/app/shared/modules/sharedcomponents/components/widget-config-axes/widget-config-axes.component.ts
+++ frontend/src/app/shared/modules/sharedcomponents/components/widget-config-axes/widget-config-axes.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+import { Subscription } from 'rxjs/Subscription';
+import { isDefaultChangeDetectionStrategy } from '@angular/core/src/change_detection/constants';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -6,7 +11,7 @@ import { Component, OnInit, HostBinding, Input, Output, EventEmitter } from '@an
     templateUrl: './widget-config-axes.component.html',
     styleUrls: []
 })
-export class WidgetConfigAxesComponent implements OnInit {
+export class WidgetConfigAxesComponent implements OnInit, OnDestroy, AfterViewInit {
     @HostBinding('class.widget-config-tab') private _hostClass = true;
     @HostBinding('class.axes-configuration') private _tabClass = true;
     @HostBinding('class.has-columns') private _modifierClass = true;
@@ -18,10 +23,164 @@ export class WidgetConfigAxesComponent implements OnInit {
     @Output() widgetChange = new EventEmitter;
 
     /** Local variables */
+    // tslint:disable-next-line:no-inferrable-types
+    y1AxisEnabled_label: string = 'enabled';
+    // tslint:disable-next-line:no-inferrable-types
+    y2AxisEnabled_label: string = 'disabled';
+    // tslint:disable-next-line:no-inferrable-types
+    xAxisEnabled_label:  string = 'disabled';
 
-    constructor() { }
+    /** Form Group */
+    widgetConfigAxes: FormGroup;
+
+    // subscriptions
+    widgetConfigAxes_Sub: Subscription;
+
+    // form values
+
+    y1AxisEnabled: any = true;
+    y1AxisUnit: any;
+    y1AxisScale: any;
+    y1AxisMin: any;
+    y1AxisMax: any;
+    y1AxisDecimals: any;
+    y1AxisLabel: any;
+
+    y2AxisEnabled: any = false;
+    y2AxisUnit: any;
+    y2AxisScale: any;
+    y2AxisMin: any;
+    y2AxisMax: any;
+    y2AxisDecimals: any;
+    y2AxisLabel: any;
+
+    xAxisEnabled: any = true;
+    xAxisMode: any;
+
+    /** Form Control Options */
+
+    yAxisWhichOptions: Array<object> = [
+        {
+            label: 'Y1',
+            value: 'y1'
+        },
+        {
+            label: 'Y2',
+            value: 'y2'
+        }
+    ];
+
+    yAxisUnitOptions: Array<object> = [
+        {
+            label: 'Kilobyte (Kb)',
+            value: 'Kb'
+        },
+        {
+            label: 'Megabyte (Mb)',
+            value: 'Mb'
+        },
+        {
+            label: 'Gigabyte (Gb)',
+            value: 'Gb'
+        }
+    ];
+
+    yAxisScaleOptions: Array<object> = [
+        {
+            label: 'Linear',
+            value: 'linear'
+        },
+        {
+            label: 'Logscale',
+            value: 'logscale'
+        }
+    ];
+
+    // NOTE: not using right now till clarification
+    xAxisModeOptions: Array<object> = [
+        {
+            label: 'Time',
+            value: 'time'
+        }
+    ];
+
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit() {
+        // populate form controls
+        this.createForm();
     }
+
+    ngAfterViewInit() {
+        // initiate any subscriptions
+    }
+
+    ngOnDestroy() {
+        // destroy any subscriptions
+        this.widgetConfigAxes_Sub.unsubscribe();
+    }
+
+    createForm() {
+        // need to actually add widget config values to form controls
+        // NOTE: exception is 'yAxisWhich' (y1 or y2), which is not a form control, and sets value on click
+
+        // ?INFO: these are mapped to the form variables set at top
+        this.widgetConfigAxes = this.fb.group({
+            y1AxisEnabled:   new FormControl(true),
+            y1AxisUnit:      new FormControl('Kb'),
+            y1AxisScale:     new FormControl('linear'),
+            y1AxisMin:       new FormControl('auto'),
+            y1AxisMax:       new FormControl('auto'),
+            y1AxisDecimals:  new FormControl('auto'),
+            y1AxisLabel:     new FormControl(),
+            y2AxisEnabled:   new FormControl(false),
+            y2AxisUnit:      new FormControl('Kb'),
+            y2AxisScale:     new FormControl('linear'),
+            y2AxisMin:       new FormControl('auto'),
+            y2AxisMax:       new FormControl('auto'),
+            y2AxisDecimals:  new FormControl('auto'),
+            y2AxisLabel:     new FormControl(),
+            xAxisEnabled:    new FormControl(true),
+            xAxisMode:       new FormControl('time')
+        });
+
+        this.widgetConfigAxes.valueChanges.distinctUntilChanged( (prev, curr) => {
+            console.log('PREVIOUS/CURRENT', prev, curr);
+            return prev === curr;
+        });
+
+        this.widgetConfigAxes.controls.y1AxisEnabled.valueChanges.subscribe(data => {
+            console.log('data', data);
+        });
+
+        // JUST CHECKING VALUES
+        this.widgetConfigAxes_Sub = this.widgetConfigAxes.valueChanges.subscribe(function(data) {
+            console.log('AXES CONFIG CHANGES', data, this.widgetConfigAxes);
+            // if (this.xAxisEnabled !== data.xAxisEnabled) {
+                this.xAxisEnabled_label = (data.xAxisEnabled) ? 'enabled' : 'disabled';
+                console.log('xAxisLabel', this.xAxisEnabled_label);
+            // }
+            // if (this.y1AxisEnabled !== data.y1AxisEnabled) {
+                this.y1AxisEnabled_label = (data.y1AxisEnabled) ? 'enabled' : 'disabled';
+                console.log('y1AxisLabel', this.y1AxisEnabled_label);
+            // }
+            // if (this.y2AxisEnabled !== data.y2AxisEnabled) {
+                this.y2AxisEnabled_label = (data.y2AxisEnabled) ? 'enabled' : 'disabled';
+                console.log('y2AxisLabel', this.y2AxisEnabled_label);
+            // }
+        }.bind(this));
+    }
+
+    /**
+     * Services
+     */
+
+     /**
+      * Events
+      */
+
+    /*click_YAxisWhichChange(val: any) {
+        this.yAxisWhich = val;
+    }*/
 
 }
--- frontend/src/app/shared/modules/sharedcomponents/components/widget-config-time/widget-config-time.component.ts
+++ frontend/src/app/shared/modules/sharedcomponents/components/widget-config-time/widget-config-time.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'widget-config-time',
     templateUrl: './widget-config-time.component.html',
-    styleUrls: []
+    styleUrls: [],
+    providers: [
+        // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+        // `MatMomentDateModule` in your applications root module. We provide it at the component level
+        // here, due to limitations of our example generation script.
+        { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    ]
 })
-export class WidgetConfigTimeComponent implements OnInit {
+export class WidgetConfigTimeComponent implements OnInit, OnDestroy, AfterViewInit {
     @HostBinding('class.widget-config-tab') private _hostClass = true;
     @HostBinding('class.time-configuration') private _tabClass = true;
     @HostBinding('class.has-columns') private _modifierClass = true;
@@ -17,11 +31,225 @@ export class WidgetConfigTimeComponent implements OnInit {
     /** Outputs */
     @Output() widgetChange = new EventEmitter;
 
-    /** Local variables */
+    /** Local Variables */
 
-    constructor() { }
+    /** Form Group */
+    widgetConfigTime: FormGroup;
+
+    // subscriptions
+    selectedDownsample_Sub: Subscription; // check formcontrol value change to see if it is 'custom'
+
+    // form values
+    selectedTimePreset: any = '1h';
+
+    customTimeRangeStart: any;
+    customTimeRangeEnd: any;
+
+    selectedAggregator: any = 'SUM';
+    timeOverTimeNumber: any = '';
+    timeOverTimePeriod: any = '';
+
+    selectedDownsample: any = 'auto';
+    customDownsampleValue: any = 0;
+    customDownsampleUnit: any = 'min';
+
+    overrideRelativeTime: any;
+    timeShift: any;
+
+    /** Form control options */
+    timePresetOptions: Array<any> = [
+        {
+            label: '1h',
+            value: '1h'
+        },
+        {
+            label: '6h',
+            value: '6h'
+        },
+        {
+            label: '12h',
+            value: '12h'
+        },
+        {
+            label: '24h',
+            value: '24h'
+        },
+        {
+            label: '2d',
+            value: '2d'
+        },
+        {
+            label: '4d',
+            value: '4d'
+        },
+        {
+            label: '7d',
+            value: '7d'
+        },
+        {
+            label: '1m',
+            value: '1m'
+        },
+        {
+            label: '3m',
+            value: '3m'
+        },
+        {
+            label: '1y',
+            value: '1y'
+        }
+    ];
+
+    timeAggregatorOptions: Array<any> = [
+        {
+            label: 'Sum',
+            value: 'SUM'
+        },
+        {
+            label: 'Min',
+            value: 'MIN'
+        },
+        {
+            label: 'Max',
+            value: 'MAX'
+        },
+        {
+            label: 'Avg',
+            value: 'AVG'
+        }
+    ];
+
+    timeDownsampleOptions: Array<any> = [
+        {
+            label: 'Auto',
+            value: 'auto'
+        },
+        {
+            label: '1 min',
+            value: '1m'
+        },
+        {
+            label: '5 min',
+            value: '5m'
+        },
+        {
+            label: '1 hr',
+            value: '1h'
+        },
+        {
+            label: '1 day',
+            value: '1d'
+        }
+    ];
+
+    timeOverTimeIterationOptions: Array<any> = [
+        {
+            label: '1',
+            value: '1'
+        },
+        {
+            label: '2',
+            value: '2'
+        },
+        {
+            label: '3',
+            value: '3'
+        },
+        {
+            label: '4',
+            value: '4'
+        },
+        {
+            label: '5',
+            value: '5'
+        },
+        {
+            label: '6',
+            value: '6'
+        },
+        {
+            label: '7',
+            value: '7'
+        }
+    ];
+
+    timeOverTimePeriodOptions: Array<any> = [
+        {
+            label: 'hours',
+            value: 'hours'
+        },
+        {
+            label: 'days',
+            value: 'days'
+        },
+        {
+            label: 'weeks',
+            value: 'weeks'
+        },
+        {
+            label: '30 days',
+            value: '30 days'
+        }
+    ];
+
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit() {
+        // populate form controls
+        this.createForm();
     }
+
+    ngAfterViewInit() {
+        // subscribe to value changes to check if 'custom' is checked
+        // so we can enable/disable the other custom fields
+        this.selectedDownsample_Sub = this.widgetConfigTime.get('selectedDownsample').valueChanges.subscribe(function(data) {
+            console.log('SELECTED DOWNSAMPLE CHANGED', data, this);
+            if (data === 'custom') {
+                this.widgetConfigTime.controls.customDownsampleValue.enable();
+                this.widgetConfigTime.controls.customDownsampleUnit.enable();
+            } else {
+                this.widgetConfigTime.controls.customDownsampleValue.disable();
+                this.widgetConfigTime.controls.customDownsampleUnit.disable();
+            }
+        }.bind(this));
+    }
+
+    ngOnDestroy() {
+        // destroy our form control subscription
+        this.selectedDownsample_Sub.unsubscribe();
+    }
+
+    createForm() {
+        // need to actually add widget config values to form controls
+        // NOTE: exception is 'time preset range', which is not a form control, and sets value on click
+
+        // ?INFO: these are mapped to the form variables set at top
+        this.widgetConfigTime = this.fb.group({
+            customTimeRangeStart:   new FormControl(),
+            customTimeRangeEnd:     new FormControl(),
+            selectedAggregator:     new FormControl(this.selectedAggregator),
+            selectedDownsample:     new FormControl(this.selectedDownsample),
+            customDownsampleValue:  new FormControl({value: 0, disabled: true}),
+            customDownsampleUnit:   new FormControl({value: 'min', disabled: true}),
+            timeOverTimeNumber:     new FormControl(),
+            timeOverTimePeriod:     new FormControl(),
+            overrideRelativeTime:   new FormControl(),
+            timeShift:              new FormControl()
+        });
+    }
+
+    /**
+     * Services
+     */
+
+
+    /**
+     * Events
+     */
+
+    click_TimePresetChange(val: any) {
+        this.selectedTimePreset = val;
+    }
+
 
 }
