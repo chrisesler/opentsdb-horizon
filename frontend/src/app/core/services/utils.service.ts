76930360fcd1bebb4418f6f71f481eb3b2973882
Hill Nguyen <hilln@oath.com>
2018-06-28 18:12:57 -0700

    function with default len param

--- frontend/src/app/core/services/utils.service.ts
+++ frontend/src/app/core/services/utils.service.ts
@@ -8,8 +8,8 @@ export class UtilsService {
   constructor() { }
 
   // random generate 6 random chars
-  generateId() {
-    return Math.random().toString(36).substring(3, 9);
+  generateId(len: number = 6) {
+    return Math.random().toString(36).substring(3, len);
   }
 
 }
