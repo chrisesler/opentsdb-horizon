de726186336967cca424a0a4aef40714789658a9
Hill Nguyen <hilln@oath.com>
2018-07-10 16:50:13 -0700

    support multiple group of data in 1 graph

--- frontend/src/app/core/services/datatranformer.service.ts
+++ frontend/src/app/core/services/datatranformer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IDygraphOptions } from '../../shared/modules/dygraphs/IDygraphOptions';
+import { isArray } from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,59 @@ import { IDygraphOptions } from '../../shared/modules/dygraphs/IDygraphOptions';
 export class DatatranformerService {
 
   constructor() { }
+// options will also be update of its labels array
+yamasToDygraph(options: IDygraphOptions, normalizedData: any[], result: any): any {  
+  let dpsHashKey = [];
+  if (normalizedData[0].length === 1) {
+    // there is no data in here but default, reset it
+    normalizedData = [];
+  } else {
+    // it has data, we need to append new data to exisiting one
+    // it only happens in case of multiple groups on same graph
 
+  }
+  let dpsHash = {};
+  // generate a hash for all the keys, might have missing time
+  // from multiple metric
+  for (let k in result) {
+    let g = result[k];
+    // build lable, it's to send exactly duplicate metric
+    let label = g.metric + ':' + Object.values(g.tags).join('-');
+    // only pushing in if not exits, since we use same reference for view/edit
+    if (!options.labels.includes(label)) {
+      options.labels.push(label);     
+    }
+    // extract date of all series to fill it up
+    for (let date in g.dps) {
+      dpsHash[date] = true
+    }
+  }
+  console.log('dpsHash', dpsHash);
+  dpsHashKey = Object.keys(dpsHash);
+  dpsHash = undefined;
+  // sort time in case  new insert somewhere
+  dpsHashKey.sort((a: any, b: any) => {
+    return a - b;
+  });
+  console.log('dpsHashKey', dpsHashKey);
+  
+  for (let idx = 0, len = dpsHashKey.length; idx < len; idx++) {
+    let dpsMs: any = dpsHashKey[idx];
+    if (!isArray(normalizedData[idx])) {
+      normalizedData[idx] = [new Date(dpsMs * 1000)];
+    }
+    for (let k in result) {
+      let g = result[k];
+      (g.dps[dpsMs] !== undefined) ? normalizedData[idx].push(g.dps[dpsMs]) : normalizedData[idx].push(null);
+    }
+  }
+  // console.log('normalizedData', options.labels, normalizedData);
+  // return normalizedData;
+  return Object.assign(normalizedData);
+}  
+/*
   // options will also be update of its labels array
-  yamasToDygraph(options: IDygraphOptions, result: any): any {
+  yamasToDygraph(options: IDygraphOptions, data: any[], result: any): void {
     let normalizedData = [];
     let dpsHash = {};
     // generate a hash for all the keys, might have missing time
@@ -42,11 +93,11 @@ export class DatatranformerService {
         (g.dps[dpsMs] !== undefined) ? normalizedData[idx].push(g.dps[dpsMs]) : normalizedData[idx].push(null);
       }
     }
-    console.log('normalizedData', options.labels, normalizedData);
+    // console.log('normalizedData', options.labels, normalizedData);
     // return normalizedData;
     return Object.assign(normalizedData);
   }
-
+*/
   // build opentsdb query base on this of full quanlify metrics for exploer | adhoc
   // defaulf time will be one hour from now
   buildAdhocYamasQuery(metrics: any[]) {
