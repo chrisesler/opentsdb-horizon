72c4b610adb78f146b77d663520a269b6da00be0
Zack Burgess <zb@yahoo-inc.com>
2018-07-19 12:02:33 -0700

    inline editor

--- frontend/src/app/shared/modules/sharedcomponents/components/inline-editable/inline-editable.component.ts
+++ frontend/src/app/shared/modules/sharedcomponents/components/inline-editable/inline-editable.component.ts
@@ -1,49 +1,73 @@
 import { Component, Input, EventEmitter, Output, ViewChild, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'inline-editable',
   templateUrl: './inline-editable.component.html',
   styleUrls: ['./inline-editable.component.scss']
 })
-export class InlineEditableComponent {
 
-  @Input() name: string;
+export class InlineEditableComponent implements OnInit {
+
   @Input() fieldValue: string;
   @Input() isRequired: boolean;
   @Input() minLength: number;
   @Input() maxLength: number;
   @Output() updateValue: EventEmitter<any> = new EventEmitter();
   @ViewChild('container') container: ElementRef;
-
-  value: any;
+  
+  fieldFormControl: FormControl;
   isEditView: boolean = false;
 
   constructor(private renderer: Renderer2) { }
 
-  showEditable() {
-     this.value = this.fieldValue;
-     this.isEditView = true;
+  ngOnInit() {
+    this.fieldFormControl = new FormControl('', []);
+    this.fieldFormControl.setValue(this.fieldValue);
+    let validators: any[] = new Array;
+  
+    if (this.isRequired) {
+      validators.push(Validators.required);
+    }
+    if (this.minLength) {
+      validators.push(Validators.minLength(this.minLength));
+    }
+    if (this.maxLength) {
+      validators.push(Validators.maxLength(this.maxLength));
+    }
+    this.fieldFormControl.setValidators(validators);
+  }
 
-    // click outside the edit zone
+  showEditable() {
+    this.isEditView = true;
+
+     // click outside the edit zone
     this.renderer.listen('document', 'click', (event) => {
       if (!this.container.nativeElement.contains(event.target)) {
-        this.isEditView = false;
+        this.resetFormField();
       }
     });
   }
 
   save() {
-    this.updateValue.emit(this.value);
-    this.fieldValue = this.value;
+    if (!this.fieldFormControl.errors) {
+      this.updateValue.emit(this.fieldFormControl.value);
+      this.fieldValue = this.fieldFormControl.value;
+      this.isEditView = false;
+    }
+  }
+
+  resetFormField() {
     this.isEditView = false;
+    this.fieldFormControl.setValue(this.fieldValue);
   }
 
   @HostListener('document:keydown', ['$event'])
   closeEditIfEscapePressed(event: KeyboardEvent) {
-      const x = event.keyCode;
-      if (x === 27) {
-        this.isEditView = false;
-      }
+    const x = event.keyCode;
+    if (x === 27) {
+      this.resetFormField();
+    }
   }
-
 }
