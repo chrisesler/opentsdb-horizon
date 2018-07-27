969092d85f467206a7ba8300611d232c8380a2e3
Zack Burgess <zb@yahoo-inc.com>
2018-07-26 21:10:59 -0700

    set prefix

--- frontend/src/app/shared/modules/sharedcomponents/components/widget-config-visual-appearance-big-number/widget-config-visual-appearance-big-number.component.ts
+++ frontend/src/app/shared/modules/sharedcomponents/components/widget-config-visual-appearance-big-number/widget-config-visual-appearance-big-number.component.ts
@@ -19,103 +19,6 @@ export class WidgetConfigVisualAppearanceBigNumberComponent implements OnInit {
     /** Local variables */
 
     selectedMetric: object;
-    // // TODO: REMOVE FAKE METRICS
-    // fakeMetrics: Array<object> = [
-    //     {
-    //         id: 0,
-    //         type: 'metric',
-    //         alias: 'M1',
-    //         label: 'Metric_namespace.app-name.whatever.some_metric',
-    //         metric: 'Metric_namespace.app-name.whatever.some_metric',
-    //         color: 'green',
-    //         collapsed: false,
-    //         visible: true,
-    //         tags: [
-    //             {
-    //                 key: 'colo',
-    //                 value: 'bf1'
-    //             },
-    //             {
-    //                 key: 'hostgroup',
-    //                 value: 'lala-01'
-    //             },
-    //             {
-    //                 key: '_aggregate',
-    //                 value: 'SUM'
-    //             }
-    //         ],
-    //         functions: [],
-    //         configuration: {
-    //             visualAppearance: {
-    //                 visualization: 'line',
-    //                 color: 'green',
-    //                 lineWeight: '2px',
-    //                 lineType: 'solid',
-    //                 logScale: false
-    //             }
-    //         }
-    //     },
-    //     {
-    //         id: 1,
-    //         type: 'metric',
-    //         alias: 'M2',
-    //         label: 'Metric_namespace.app-name.something.some_metric',
-    //         metric: 'Metric_namespace.app-name.something.some_metric',
-    //         color: 'amber',
-    //         collapsed: false,
-    //         visible: true,
-    //         tags: [
-    //             {
-    //                 key: 'colo',
-    //                 value: 'bf1'
-    //             },
-    //             {
-    //                 key: 'hostgroup',
-    //                 value: 'hg-01'
-    //             }
-    //         ],
-    //         functions: [],
-    //         configuration: {
-    //             visualAppearance: {
-    //                 visualization: 'line',
-    //                 color: 'amber',
-    //                 lineWeight: '2px',
-    //                 lineType: 'solid',
-    //                 logScale: false
-    //             }
-    //         }
-    //     },
-    //     {
-    //         id: 1,
-    //         type: 'expression',
-    //         alias: 'E1',
-    //         label: 'expression-name',
-    //         expression: 'm1 + m2 / m2',
-    //         color: 'fuchsia',
-    //         collapsed: false,
-    //         visible: true,
-    //         tags: [
-    //             {
-    //                 key: 'colo',
-    //                 value: '*'
-    //             },
-    //             {
-    //                 key: 'hostgroup',
-    //                 value: '*'
-    //             }
-    //         ],
-    //         functions: [],
-    //         configuration: {
-    //             visualAppearance: {
-    //                 visualization: 'line',
-    //                 color: 'fuschia',
-    //                 lineWeight: '2px',
-    //                 lineType: 'solid',
-    //                 logScale: false
-    //             }
-    //         }
-    //     }
-    // ];
 
     setSelectedMetric(metric) {
         this.selectedMetric = metric;
@@ -124,6 +27,15 @@ export class WidgetConfigVisualAppearanceBigNumberComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
+        this.selectedMetric = this.widget[0];
+    }
+
+    KeyedOnPrefixInputBox(value: string) {
+        this.selectedMetric['configuration']['bigNum']['prefix'] = value;
+    }
+
+    selectedPrefixSize(value: string) {
+        this.selectedMetric['configuration']['bigNum']['prefixSize'] = value;
     }
 
 }
