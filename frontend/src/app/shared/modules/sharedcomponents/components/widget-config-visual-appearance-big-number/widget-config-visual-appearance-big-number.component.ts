dfa443137c3fc7275936926e62a45ae67d1c3108
Zack Burgess <zb@yahoo-inc.com>
2018-07-27 12:41:06 -0700

    set transparent background color

--- frontend/src/app/shared/modules/sharedcomponents/components/widget-config-visual-appearance-big-number/widget-config-visual-appearance-big-number.component.ts
+++ frontend/src/app/shared/modules/sharedcomponents/components/widget-config-visual-appearance-big-number/widget-config-visual-appearance-big-number.component.ts
@@ -66,7 +66,23 @@ export class WidgetConfigVisualAppearanceBigNumberComponent implements OnInit {
             this.selectedMetric['configuration']['bigNum']['textColor'] = color['hex'];
         } else { // background
             this.selectedMetric['configuration']['bigNum']['backgroundColor'] = color['hex'];
+            this.selectedMetric['configuration']['bigNum']['backgroundColorTransparent'] = this.hexToTransparentHex(color['hex']);
+            this.selectedMetric['configuration']['bigNum']['backgroundLuma'] = this.hexToLuma(color['hex']);
         }
     }
 
+    // TODO: remove duplicate from BigNumber.ts
+    hexToTransparentHex(hex: string): string {
+        return hex + '80'; // 80 is 50% in hex
+    }
+
+    // TODO: remove duplicate from BigNumber.ts
+    hexToLuma(hex: string): number {
+        const bigint = parseInt(hex.substring(1), 16);
+        // tslint:disable:no-bitwise
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+        return 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    }
 }
