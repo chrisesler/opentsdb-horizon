fc59d6fb1031139bd4b38944168779062b6083e4
Hill Nguyen <hilln@oath.com>
2018-07-30 16:07:41 -0700

    add utils function

--- frontend/src/app/core/services/utils.service.ts
+++ frontend/src/app/core/services/utils.service.ts
@@ -13,4 +13,18 @@ export class UtilsService {
     return Math.random().toString(36).substring(start, len + start);
   }
 
+  modifyWidgets(dashboard: any) {
+    // add extra info item behaviors
+    console.log('passing dashbaord', dashboard);
+    
+    for (let i = 0; i < dashboard.widgets.length; i++) {
+      const wd: any = dashboard.widgets[i];
+      //wd.id = this.utils.generateId(); // we set it manually to test
+      const mod = { 'xMd': wd.gridPos.x, 'yMd': wd.gridPos.y, 'dragAndDrop': true, 'resizable': true };
+      wd.gridPos = { ...wd.gridPos, ...mod };
+    }
+    console.log('modified dashbaord', dashboard);
+    //return dashboard;
+  }
+
 }
