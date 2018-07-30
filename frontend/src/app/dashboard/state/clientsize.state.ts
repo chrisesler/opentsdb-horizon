fd06483cd2b4505898b639fb2ec5f073bfdfd1f7
Hill Nguyen <hilln@oath.com>
2018-07-30 15:59:18 -0700

    define client size state, size for 1 grister unit

---
+++ frontend/src/app/dashboard/state/clientsize.state.ts
@@ -0,0 +1,33 @@
+import { State, StateContext, Selector, Action } from '@ngxs/store';
+
+export interface GridsterUnitSizeModel {
+    width: number;
+    height: number;
+}
+
+// action
+export class UpdateGridsterUnitSize {
+    public static type = '[Widget] Update Widget Size';
+    constructor(public readonly payload: any) {}
+}
+
+@State<GridsterUnitSizeModel>({
+    name: 'ClientSize',
+    defaults: {
+        width: 120,
+        height: 80
+    }
+})
+
+export class ClientSizeState {
+
+    @Selector() static getUpdatedGridsterUnitSize(state: GridsterUnitSizeModel) {
+        return {...state};
+    }
+
+    @Action(UpdateGridsterUnitSize)
+    updateGridsterUnitSize(ctx: StateContext<GridsterUnitSizeModel>, { payload }: UpdateGridsterUnitSize) {
+        const state = ctx.getState();
+        ctx.setState({...state, ...payload});
+    }
+}
