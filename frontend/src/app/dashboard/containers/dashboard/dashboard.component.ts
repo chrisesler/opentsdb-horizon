a427e59bb90c48068a718f0a3cc33d452f9a51c6
Hill Nguyen <hilln@oath.com>
2018-07-30 16:07:10 -0700

    adding new substate and selectors

--- frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/containers/dashboard/dashboard.component.ts
@@ -8,11 +8,15 @@ import { DashboardService } from '../../services/dashboard.service';
 import { IntercomService, IMessage } from '../../../core/services/intercom.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Store, Select } from '@ngxs/store';
-import { DashboardState } from '../../state/dashboard.state';
 import { AuthState } from '../../../shared/state/auth.state';
 import { Observable } from 'rxjs';
 import { ISelectedTime } from '../../../shared/modules/date-time-picker/models/models';
-import * as dashboardActions from '../../state/dashboard.actions';
+
+import { DBState, LoadDashboard } from '../../state/dashboard.state';
+import { WidgetsState, LoadWidgets, UpdateGridPos, WidgetModel} from '../../state/widgets.state';
+import { WidgetsRawdataState, GetQueryDataByGroup } from '../../state/widgets-data.state';
+import { ClientSizeState, UpdateGridsterUnitSize } from '../../state/clientsize.state';
+import { DBSettingsState, UpdateMode} from '../../state/settings.state';
 
 import { MatMenu, MatMenuTrigger } from '@angular/material';
 
@@ -24,13 +28,15 @@ import { MatMenu, MatMenuTrigger } from '@angular/material';
 export class DashboardComponent implements OnInit, OnDestroy {
 
     @HostBinding('class.app-dashboard') private hostClass = true;
-    // @Select(state => state.dashboardState.widgets) widgets$: Observable<any>;
-    @Select(DashboardState.getWidgets) widgets$: Observable<any>;
-    @Select(DashboardState.setViewEditMode) viewEditMode$: Observable<any>;
-    @Select(AuthState.getAuth) auth$: Observable<string>;
-    // @Select(DashboardState.getUpdatedWidgetId) updatedWidgetId$: Observable<string>;
-    @Select(DashboardState.getWidgetGroupUpdate) updatedWidgetGroup$: Observable<any>;
 
+    @Select(AuthState.getAuth) auth$: Observable<string>;
+    // new state
+    @Select(DBState.getLoadedDB) loadedRawDB$: Observable<any>;
+    @Select(WidgetsState.getWigets) widgets$: Observable<WidgetModel[]>;
+    @Select(WidgetsRawdataState.getLastModifiedWidgetRawdata) widgetRawData$: Observable<any>;
+    @Select(WidgetsRawdataState.getLastModifiedWidgetRawdataByGroup) widgetGroupRawData$: Observable<any>;
+    @Select(ClientSizeState.getUpdatedGridsterUnitSize) gridsterUnitSize$: Observable<any>;
+    @Select(DBSettingsState.GetDashboardMode) dashboardMode$: Observable<string>;
     // dashboard action menu trigger
     @ViewChild(MatMenuTrigger) actionMenuTrigger: MatMenuTrigger;
 
@@ -67,10 +73,11 @@ export class DashboardComponent implements OnInit, OnDestroy {
                 if (this.dbid === '_new_') {
                     console.log('creating a new dashboard...');
                     const newdboard = this.dbService.getDashboardPrototype();
-                    this.store.dispatch(new dashboardActions.CreateNewDashboard(newdboard));
+                    //this.store.dispatch(new dashboardActions.CreateNewDashboard(newdboard));
                 } else {
                     // load provided dashboard id, and need to handdle not found too
-                    this.store.dispatch(new dashboardActions.LoadDashboard(this.dbid));
+                    //this.store.dispatch(new dashboardActions.LoadDashboard(this.dbid));
+                    this.store.dispatch(new LoadDashboard(this.dbid));
                 }
             }
         });
@@ -83,33 +90,63 @@ export class DashboardComponent implements OnInit, OnDestroy {
             //  console.log('listen to: ', JSON.stringify(message));
             switch (message.action) {
                 case 'viewEditMode':
-                    this.store.dispatch(new dashboardActions.SetViewEditMode(message.payload));
+                    //this.store.dispatch(new dashboardActions.SetViewEditMode(message.payload));
                     break;
                 case 'removeWidget':
-                    this.store.dispatch(new dashboardActions.RemoveWidget(message.payload));
+                    //this.store.dispatch(new dashboardActions.RemoveWidget(message.payload));
                     this.rerender = { 'reload': true };
                     break;
                 case 'closeViewEditMode':
-                    this.store.dispatch(new dashboardActions.SetViewEditMode(message.payload));
+                    //this.store.dispatch(new dashboardActions.SetViewEditMode(message.payload));
                     this.rerender = { 'reload': true };
                     break;
                 case 'getQueryData':
                     console.log('the query: ', message.payload);
                     // payload needs to break into group to send in
-                    this.handleQueryPayload(message);
-                    // comment below line for now
-                    // this.store.dispatch(new dashboardActions.GetQueryData(message.id, message.payload));                
+                    this.handleQueryPayload(message);              
                     break;
                 default:
                     break;
             }
         });
+        
+        this.loadedRawDB$.subscribe( db => {
+            // update WidgetsState
+            this.store.dispatch(new LoadWidgets(db.widgets));
+        });
+
+        this.widgetRawData$.subscribe(result => {
+                   
+        });
+
+        this.widgetGroupRawData$.subscribe(result => {
+            if (result !== undefined) {
+                this.interCom.responsePut({
+                    id: result.wid,
+                    action: 'updatedWidgetGroup',
+                    payload: { 
+                        gid: result.gid, 
+                        rawdata: result.rawdata
+                    }
+                }); 
+            }           
+        });
+
+        // all widgets should update their own size
+        this.gridsterUnitSize$.subscribe( unitSize => {
+            this.interCom.responsePut({
+                action: 'resizeWidget',
+                payload: unitSize
+            });
+        });
 
         // after success loaded dashboard, assigned widgets
+        /*
         this.widgets$.subscribe(widgets => {
             console.log('widgets$ is calling');   
             this.widgets = widgets;
         });
+        */
         // when an widget is updated by getting raw data, based on its component type
         // we need to transform data to its data format to required format of wdget visualization to render
         // transormation can be done here in dashboad service and passing back to data.
@@ -131,6 +168,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
        // when the data is updated, store will update, we use this selector to listen to
        // update from store after getting data for each group, at this point return rawdata
        // is already added to state
+       /*
        this.updatedWidgetGroup$.subscribe(wg => {
             console.log('updateWidgetGroup$ calling', wg);
            // only ask widget to tranform data if they not did it yes
@@ -147,8 +185,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
             }
            }
        });
-
+       */
         // sending down view edit mode to handle size
+        /*
         this.viewEditMode$.subscribe(payload => {
             console.log('viewEditMode$ calling', payload);
             this.viewEditMode = payload.editMode;
@@ -158,7 +197,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
                 payload: payload
             });
         });
-
+        */
         this.auth$.subscribe(auth => {
             console.log('auth$ calling', auth);
             if (auth === 'invalid') {
@@ -169,48 +208,44 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     // dispatch payload query by group
     handleQueryPayload(message: any) {
-        let widgetid = message.id;
         let groupid = '';
         let payload = message.payload;
         
         for (let i = 0; i < payload.groups.length; i++) {
             let group: any = payload.groups[i];            
             groupid = group.id;
+            // format for opentsdb query
             let query: any = {
                 start: payload.start,
                 end: payload.end,
                 downsample: payload.downsample,
                 queries: group.queries
             };
-            console.log('the group query', query);          
+            console.log('the group query', query);   
+            let gquery = {
+                wid: message.id,
+                gid: groupid,
+                query: query
+            };       
             // now dispatch request
-            this.store.dispatch(new dashboardActions.GetQueryDataByGroup(widgetid, groupid, query));
+            this.store.dispatch(new GetQueryDataByGroup(gquery));
         }
     }
 
     // this will call based on gridster reflow and size changes event
-    widgetsLayoutUpdate(widgets: any[]) {
-        console.log('WidgetsLayoutUpdate is calling');       
-        this.store.dispatch(new dashboardActions.UpdateWidgetsLayout({ widgets }));
-        // we the broadcast new dimention down to them for resizing
-        for (let i = 0; i < widgets.length; i++) {
-            this.interCom.responsePut({
-                id: widgets[i].id,
-                action: 'resizeWidget',
-                payload: widgets[i].clientSize
-            }
-            );
+    widgetsLayoutUpdate(gridLayout: any) {
+        if (gridLayout.clientSize) {
+            this.store.dispatch(new UpdateGridsterUnitSize(gridLayout.clientSize));
+        }
+        if (gridLayout.wgridPos) {
+            this.store.dispatch(new UpdateGridPos(gridLayout.wgridPos));
         }
     }
-
-    /**
-     * Behaviors
-     */
 
     // event emit to add new widget from dashboard header
     addNewWidget() {
         const payload = { widget: this.dbService.getWidgetPrototype() };
-        this.store.dispatch(new dashboardActions.AddWidget(payload));
+        //this.store.dispatch(new dashboardActions.AddWidget(payload));
         // trigger Update Widget layout event
         this.rerender = { 'reload': true };
     }
@@ -244,6 +279,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
         this.listenSub.unsubscribe();
         this.routeSub.unsubscribe();
         // we need to clear dashboard state
-        this.store.dispatch(new dashboardActions.ResetDashboardState);    
+        //this.store.dispatch(new dashboardActions.ResetDashboardState);    
     }
 }
