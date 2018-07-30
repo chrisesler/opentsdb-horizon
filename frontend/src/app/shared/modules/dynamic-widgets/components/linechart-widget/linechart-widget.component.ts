7b55d796c32758dd70aa648f7095280b1ebbfbbf
Hill Nguyen <hilln@oath.com>
2018-07-30 16:06:48 -0700

    working new state and actions

--- frontend/src/app/shared/modules/dynamic-widgets/components/linechart-widget/linechart-widget.component.ts
+++ frontend/src/app/shared/modules/dynamic-widgets/components/linechart-widget/linechart-widget.component.ts
@@ -3,7 +3,7 @@ import { IntercomService, IMessage } from '../../../../../core/services/intercom
 import { DatatranformerService } from '../../../../../core/services/datatranformer.service';
 
 import { Subscription } from 'rxjs/Subscription';
-import { WidgetModel } from '../../../../../dashboard/state/dashboard.state';
+import { WidgetModel } from '../../../../../dashboard/state/widgets.state';
 import { IDygraphOptions } from '../../../dygraphs/IDygraphOptions';
 
 @Component({
@@ -36,6 +36,7 @@ export class LinechartWidgetComponent implements OnInit, OnChanges, AfterViewIni
         drawPoints: false,
         labelsDivWidth: 0,
         legend: 'never',
+        logscale: true,
         stackedGraph: this.isStackedGraph,
         hightlightCircleSize: 1,
         strokeWidth: 1,
@@ -59,28 +60,21 @@ export class LinechartWidgetComponent implements OnInit, OnChanges, AfterViewIni
     ngOnInit() {
         // subscribe to event stream
         this.listenSub = this.interCom.responseGet().subscribe((message: IMessage) => {
+
+            if (message.action === 'resizeWidget') {
+                // we get the size to update the graph size
+                this.size = { width: message.payload.width * this.widget.gridPos.w, 
+                                height: message.payload.height * this.widget.gridPos.h
+                            };
+            }
+
             if (message && (message.id === this.widget.id)) {
                 switch (message.action) {
-                    case 'resizeWidget':
-                        // we get the size to update the graph size
-                        this.size = { width: message.payload.width, height: message.payload.height };
-                        break;
-                    case 'updatedWidgetGroup':
-                        console.log('update widget group', message, this.widget);
-                        //console.log('this options label', this.options);
-                        
+                    case 'updatedWidgetGroup':                        
                         if (this.widget.id === message.id) {
-                            //this.isDataLoaded = true; // need to handle the case that it's partial of groups
-                            // handle case that nav out of dashboard, and state is kinda reset.
-                            if (this.widget.rawdata[message.payload]) {
-                                let grawdata = this.widget.rawdata[message.payload];
-                                this.data = this.dataTransformer.yamasToDygraph(this.options, this.data, grawdata); 
-                            console.log('datata-' + this.widget.id, this.data.legend); 
-                            } else {
-                                console.log('no data for ', message.payload);
-                                
-                            }
-                                                                               
+                            this.isDataLoaded = true;
+                            let rawdata = message.payload.rawdata;                            
+                            this.data = this.dataTransformer.yamasToDygraph(this.options, this.data, rawdata);                                                                                                   
                         }
                         break;
                     // this case might be removed since we now get data by group
@@ -95,6 +89,7 @@ export class LinechartWidgetComponent implements OnInit, OnChanges, AfterViewIni
                         break;
                     case 'viewEditWidgetMode':
                         console.log('vieweditwidgetmode', message, this.widget);
+                        /*
                         if (this.widget.id === message.id) {
                             this.isDataLoaded = true;
                             this.data = this.dataTransformer.yamasToDygraph(this.options, this.data, this.widget.data.rawdata);
@@ -103,6 +98,7 @@ export class LinechartWidgetComponent implements OnInit, OnChanges, AfterViewIni
                             let nHeight = this.widgetOutputElement.nativeElement.offsetHeight;
                             this.size = { width: nWidth + 20, height: nHeight - 60 };
                         }
+                        */
                         break;
                 }
             }
@@ -125,7 +121,7 @@ export class LinechartWidgetComponent implements OnInit, OnChanges, AfterViewIni
     }
 
     ngAfterViewInit() {
-        console.log('TEST', this.widgetOutputElement.nativeElement.getBoundingClientRect());
+        //console.log('TEST', this.widgetOutputElement.nativeElement.getBoundingClientRect());
     }
 
     /**
