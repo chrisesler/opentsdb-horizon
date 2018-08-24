442b9220aa712b787a8b665fdc6746082f958f43
Chris Esler <cesler@oath.com>
2018-08-24 15:06:22 -0700

    Side Nav start, placement and starting to wire up some behavior events. Additionally, finally changing over to Heveltica Neue for primary font, and Roboto-Mono for the monospace font.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,32 +1,78 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, ViewChild } from '@angular/core';
 import { AuthState } from './shared/state/auth.state';
 import { Observable } from 'rxjs';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSidenav } from '@angular/material';
 import { LoginExpireDialogComponent } from './core/components/login-expire-dialog/login-expire-dialog.component';
 import { Select } from '@ngxs/store';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [
-    './app.component.scss'
-  ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [
+        './app.component.scss'
+    ]
 })
 export class AppComponent implements OnInit {
-  @HostBinding('class.app-root') hostClass = true;
-  @Select(AuthState.getAuth) auth$: Observable<string>;
+    @HostBinding('class.app-root') hostClass = true;
+    @Select(AuthState.getAuth) auth$: Observable<string>;
 
-  title = 'app';
+    /** View Childs */
+    @ViewChild(MatSidenav) sideNav: MatSidenav;
 
-  constructor(private dialog: MatDialog) {  }
-  ngOnInit() {
-    this.auth$.subscribe(auth => {
-        if ( auth === 'invalid' ) {
-            console.log('open auth dialog');
-            this.dialog.open(LoginExpireDialogComponent);
-        } else if ( auth === 'valid' ) {
-            this.dialog.closeAll();
+    /** Local variables */
+    title = 'app';
+
+    // sidenav variables
+    private _sidenavOpen: boolean;
+
+    set sidenavOpen(open: boolean) {
+        this._sidenavOpen = open;
+    }
+
+    get sidenavOpen(): boolean {
+        return this._sidenavOpen;
+    }
+
+    constructor(private dialog: MatDialog) { }
+
+    ngOnInit() {
+        this.auth$.subscribe(auth => {
+            if (auth === 'invalid') {
+                console.log('open auth dialog');
+                this.dialog.open(LoginExpireDialogComponent);
+            } else if (auth === 'valid') {
+                this.dialog.closeAll();
+            }
+        });
+    }
+
+    /**
+     * SIDE NAV ITEMS
+     */
+
+    toggleSidenav() {
+        this.sidenavOpen = !this.sidenavOpen;
+
+        // if it is open
+        if (this.sidenavOpen) {
+            this.sideNav.open();
+
+            // timer setup
+
+        } else {
+            this.sideNav.close();
         }
-    });
-  }
+    }
+
+    sidenavMouseover(e: any) {
+        console.log('%cEVENT: Mouse Over', 'color: #fff, background: red; font-weight: bold;', e);
+    }
+
+    sidenavMouseout(e: any) {
+        console.log('%cEVENT: Mouse Out', 'color: #fff, background: red; font-weight: bold;', e);
+    }
+
+    sidenavMousemove(e: any) {
+        console.log('%cEVENT: Mouse Over', 'color: #fff, background: red; font-weight: bold;', e);
+    }
 }
