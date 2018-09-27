e1e2daadace388af4251a9d5ee00d563e08b650b
Chris Esler <cesler@oath.com>
2018-09-27 14:22:52 -0700

    adding some widget component inex.ts in order to load more efficiently

---
+++ frontend/src/app/shared/modules/dynamic-widgets/components/bignumber-widget/index.ts
@@ -0,0 +1,3 @@
+export * from './bignumber-widget.component';
+export * from './children/big-number-visual-appearance/big-number-visual-appearance.component';
+export * from './children/bignumber-config-metric-queries/bignumber-config-metric-queries.component';
