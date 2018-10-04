54bac2368138c965c77f238093ae8f2a1d6b303e
Hill Nguyen <hilln@oath.com>
2018-10-04 15:47:37 -0700

    testing out the build

--- yahoo/publish.sh
+++ yahoo/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-yinst_create -t release -clean -target_dir $PUBLISH_DIR  $SOURCE_DIR/yahoo/horizon.yicf
-dist_install -batch -identity ~/.ssh/rsa_dist -headless -branch test -nomail $PUBLISH_DIR/*.tgz
+yinst_create --debug -t release -clean -target_dir $PUBLISH_DIR  $SOURCE_DIR/yahoo/horizon.yicf
+yes | dist_install -batch -identity ~/.ssh/rsa_dist -headless -branch test -nomail $PUBLISH_DIR/*.tgz
