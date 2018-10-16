f78561269cfb0cd5cd184d6dc8f999e6b03f12b0
Zack Burgess <zb@yahoo-inc.com>
2018-10-16 12:34:11 -0700

    updates for Hill

--- frontend/src/app/shared/modules/chartjs/components/chartjs.directive.ts
+++ frontend/src/app/shared/modules/chartjs/components/chartjs.directive.ts
@@ -3,9 +3,7 @@ import { OnInit, OnChanges, OnDestroy, Directive,
 import 'chart.js';
 import * as thresholdPlugin from '../../../chartjs-threshold-plugin/src/index';
 import { UnitConverterService } from '../../../../core/services/unit-converter.service';
-// import 'chartjs-plugin-labels';
-
-
+import 'chartjs-plugin-labels';
 
 @Directive({
   selector: '[chartjs]'
