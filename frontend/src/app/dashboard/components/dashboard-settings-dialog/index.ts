7fd9b0874542116472ec95fd4961571a12447c05
Chris Esler <cesler@oath.com>
2018-10-27 10:56:41 -0700

    adding child to rollup

--- frontend/src/app/dashboard/components/dashboard-settings-dialog/index.ts
+++ frontend/src/app/dashboard/components/dashboard-settings-dialog/index.ts
@@ -1,3 +1,4 @@
+export * from './children/dbs-variable-item/dbs-variable-item.component';
 export * from './children/dbs-json/dbs-json.component';
 export * from './children/dbs-meta/dbs-meta.component';
 export * from './children/dbs-variables/dbs-variables.component';
