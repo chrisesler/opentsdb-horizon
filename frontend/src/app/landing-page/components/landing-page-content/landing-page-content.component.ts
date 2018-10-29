e74de5aa7199a58d8fecb6fc6040f8e2de5f3e39
Hill Nguyen <hilln@oath.com>
2018-10-29 14:48:28 -0700

    update link to create a new dashboard

--- frontend/src/app/landing-page/components/landing-page-content/landing-page-content.component.ts
+++ frontend/src/app/landing-page/components/landing-page-content/landing-page-content.component.ts
@@ -123,7 +123,7 @@ export class LandingPageContentComponent implements OnInit {
     }
 
     createDashboard() {
-        this.router.navigate(['dashboard', '_new_']);
+        this.router.navigate(['d', '_new_']);
     }
 
     // TODO: Get this link to yamas guide to work
