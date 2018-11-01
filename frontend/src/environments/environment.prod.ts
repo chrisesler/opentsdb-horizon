9a5f78892512baa32ae6925a8125d7d5e31157f3
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2018-11-01 10:17:02 -0700

    updating configdb url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   tsdb_host: 'https://tsdbr-1-gq1.yamas.ops.yahoo.com:4443',
-  configdb: 'http://stg-horizon-service-1.yms.gq1.yahoo.com:4080/api/v1'
+  configdb: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1'
 };
