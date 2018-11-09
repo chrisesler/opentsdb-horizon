90e0c67682afa205f30c51079cea0b1d23bd931c
Chris Esler <cesler@oath.com>
2018-11-09 15:33:23 -0800

    directive to panel item. purely gets the content template into a easy variable

---
+++ frontend/src/app/app-shell/directives/navigator-panel-item.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NavigatorPanelItemDirective } from './navigator-panel-item.directive';
+
+describe('NavigatorPanelItemDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NavigatorPanelItemDirective();
+    expect(directive).toBeTruthy();
+  });
+});
