d408d090adaa101f004fcb2d615b0f3977e3de15
Chris Esler <cesler@oath.com>
2018-12-05 15:13:13 -0800

    environment to update to latest instance... don't know yet if I can move configdb2 to just configdb. will confirm shortly

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -8,5 +8,5 @@ export const environment = {
   // tsdb_host: 'https://tsdbr-1-gq1.yamas.ops.yahoo.com:4443',
   tsdb_host: 'https://tsdbr-1-gq1.yamas.ops.yahoo.com:4443',
   configdb: 'http://stg-horizon-service-1.yms.gq1.yahoo.com:4080/api/v1',
-  configdb2: 'https://qa-horizonapi.yamas.ouroath.com:4443/api/v1'
+  configdb2: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1'
 };
