0bb47a93c9b1850bfff4c66ac7abad5916e3fc82
Chris Esler <cesler@oath.com>
2018-12-07 11:45:23 -0800

    Empty panels setups for 'coming soon' messages on the navigator

--- frontend/src/app/app-shell/components/test-navigator/test-navigator.component.ts
+++ frontend/src/app/app-shell/components/test-navigator/test-navigator.component.ts
@@ -4,27 +4,41 @@ import {
     HostBinding,
     Input,
     OnInit,
-    Output
+    Output, ViewChild, ElementRef
 } from '@angular/core';
+
+import { NavigatorPanelComponent } from '../navigator-panel/navigator-panel.component';
+
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'test-navigator',
     templateUrl: './test-navigator.component.html',
-    styleUrls: []
+    styleUrls: ['./test-navigator.component.scss']
 })
 export class TestNavigatorComponent implements OnInit {
 
     @HostBinding('class.test-navigator') private _hostClass = true;
 
+    @ViewChild(NavigatorPanelComponent) private navPanel: NavigatorPanelComponent;
+
     @Input() activeNavSection: any = '';
     @Input() drawerMode: any = 'over';
+
+    @Input() panelTitle: any = '';
+    @Input() panelText: any = '';
 
     @Output() toggleDrawer: EventEmitter<any> = new EventEmitter();
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    closeDrawer() {
+        this.toggleDrawer.emit({
+            closeNavigator: true
+        });
     }
 
     toggleDrawerMode() {
