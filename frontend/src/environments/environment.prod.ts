307d6e6a78e636c1edd705722beaa96d5538752b
Hill Nguyen <hilln@oath.com>
2019-01-28 14:57:26 -0800

    update new cname hosts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
 export const environment = {
   production: true,
   tsdb_host: 'https://tsdbr-1-bf2.yamas.ouroath.com:4443',
+  tsdb_hosts: [
+    'metrics-a.yamas.ouroath.com:4443',
+    'metrics-b.yamas.ouroath.com:4443',
+    'metrics-c.yamas.ouroath.com:4443',
+    'metrics-d.yamas.ouroath.com:4443',
+    'metrics-e.yamas.ouroath.com:4443',
+    'metrics-f.yamas.ouroath.com:4443',
+    'metrics-g.yamas.ouroath.com:4443',
+  ],
   configdb: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1',
   configdb2: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1'
 };
