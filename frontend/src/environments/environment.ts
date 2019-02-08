b04bb7e26171de69e5f00b2520885c46ee0145cc
Hill Nguyen <hilln@oath.com>
2019-02-08 11:07:39 -0800

    roll back the changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
     'https://metrics-f.yamas.ouroath.com:443',
     'https://metrics-g.yamas.ouroath.com:443',
   ],
-  configdb: 'https://qa-horizonapi.yamas.ouroath.com:4443/api/v1',
+  configdb: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1',
   configdb2: 'https://stg-horizonapi.yamas.ouroath.com:4443/api/v1',
   // metaApi: 'https://tsdbr-20-gq1.yamas.ouroath.com:4443/api'
   metaApi: 'https://meta.yamas.ouroath.com:443/api'
