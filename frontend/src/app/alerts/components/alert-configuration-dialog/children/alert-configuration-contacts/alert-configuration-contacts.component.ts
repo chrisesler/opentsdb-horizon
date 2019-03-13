4fd7576691e83d9415cd489b0f07eb98f2b8a87d
Zack Burgess <zb@yahoo-inc.com>
2019-03-13 10:00:48 -0700

    use ids for contacts

--- frontend/src/app/alerts/components/alert-configuration-dialog/children/alert-configuration-contacts/alert-configuration-contacts.component.ts
+++ frontend/src/app/alerts/components/alert-configuration-dialog/children/alert-configuration-contacts/alert-configuration-contacts.component.ts
@@ -21,43 +21,43 @@ export class AlertConfigurationContactsComponent implements OnInit {
 
   visible = true;
   selectable = true;
-  removable = true;
   addOnBlur = true;
+  contactRemovable: boolean;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  selectedContacts: any = [];
+  selectedContacts: any = []; // list of id's
   exisitingContacts: any = [
     {
+        id: '1',
         name: 'prod',
-        type: 'Group',
-        isSelected: false
+        type: 'Group'
     },
     {
+        id: '2',
         name: 'dev-team',
-        type: 'OpsGenie',
-        isSelected: false
+        type: 'OpsGenie'
     },
     {
+        id: '3',
         name: 'yamas-devel',
-        type: 'Slack',
-        isSelected: false
+        type: 'Slack'
     },
     {
+        id: '4',
         name: 'yamas-devel@verizonmedia.com',
-        type: 'Email',
-        isSelected: false
+        type: 'Email'
     }
 ];
 
   ngOnInit() {
+    this.contactRemovable = this.moreThanOneContactSelected();
   }
-
 
   focusFunction() {
     this.contactsFocused = true;
   }
 
-  focusOutFunction() {
+  collapsePanel() {
       this.contactsFocused = false;
       this.addContact = 'existing';
   }
@@ -79,98 +79,94 @@ export class AlertConfigurationContactsComponent implements OnInit {
       this.addContact = 'opsgenie';
   }
 
+  // TODO: remove
   latestContactName(name: string) {
     this.lastContactName = name;
   }
 
   addGroupContact() {
-      // tslint:disable:prefer-const
-      let contact: any = {};
-      contact.name = this.lastContactName;
-      contact.type = 'Group';
-      contact.isSelected = false;
-      this.addToExistingContacts(contact);
-      this.viewExistingContacts();
+    // tslint:disable:prefer-const
+    let contact: any = {};
+    contact.name = this.lastContactName;
+    contact.type = 'Group';
+    contact.id = this.exisitingContacts[this.exisitingContacts.length - 1].id + 1;  // TODO: get from server
+    this.addToExistingContacts(contact);
+    this.viewExistingContacts();
   }
 
   addOpsGenieContact() {
-      let contact: any = {};
-      contact.name = this.lastContactName;
-      contact.type = 'OpsGenie';
-      contact.isSelected = false;
-      this.addToExistingContacts(contact);
-      this.viewExistingContacts();
+    let contact: any = {};
+    contact.name = this.lastContactName;
+    contact.type = 'OpsGenie';
+    contact.id = this.exisitingContacts[this.exisitingContacts.length - 1].id + 1; // TODO: get from server
+    this.addToExistingContacts(contact);
+    this.viewExistingContacts();
   }
 
   addSlackContact() {
-      let contact: any = {};
-      contact.name = this.lastContactName;
-      contact.type = 'Slack';
-      contact.isSelected = false;
-      this.addToExistingContacts(contact);
-      this.viewExistingContacts();
+    let contact: any = {};
+    contact.name = this.lastContactName;
+    contact.type = 'Slack';
+    contact.id = this.exisitingContacts[this.exisitingContacts.length - 1].id + 1; // TODO: get from server
+    this.addToExistingContacts(contact);
+    this.viewExistingContacts();
   }
 
-  addEmailContact(email: string, isSelected): any {
+  addEmailContact(email: string): any {
     let contact: any = {};
     contact.name = email;
     contact.type = 'Email';
-    contact.isSelected = isSelected;
+    contact.id = this.exisitingContacts[this.exisitingContacts.length - 1].id + 1; // TODO: get from server
     this.addToExistingContacts(contact);
     return contact;
   }
 
   addToExistingContacts(contact: any) {
-    // contact.isSelected = false;
     this.exisitingContacts.push(contact);
 }
 
   getGroupContacts(): any[] {
-      // console.log(this.getContacts('Group'));
-      return this.getContacts('Group');
+    return this.getContacts('Group');
   }
 
   getOpsGenieContacts(): any[] {
-      return this.getContacts('OpsGenie');
+    return this.getContacts('OpsGenie');
   }
 
   getEmailContacts(): any[] {
-      return this.getContacts('Email');
+    return this.getContacts('Email');
   }
 
   getSlackContacts(): any[] {
-      return this.getContacts('Slack');
+    return this.getContacts('Slack');
   }
 
   getContacts(type: string): any[] {
-      // tslint:disable:prefer-const
-      let contacts = [];
-      for (let contact of this.exisitingContacts) {
-          if (contact.type === type) {
-              contacts.push(contact);
-          }
-      }
-      return contacts;
-  }
-
-  addContactToSelectedContacts(contact: any) {
-    this.addContactFromName(contact.name);
+    // tslint:disable:prefer-const
+    let contacts = [];
+    for (let contact of this.exisitingContacts) {
+        if (contact.type === type) {
+            contacts.push(contact);
+        }
+    }
+    return contacts;
   }
 
   addContactFromName(contactName: string) {
     let isExistingContact: boolean = false;
 
-    for (let _contact of this.exisitingContacts) {
-      if (_contact.name === contactName) {
-        _contact.isSelected = true;
-        this.selectedContacts.push(_contact);
+    for (let contact of this.exisitingContacts) {
+      if (isExistingContact && contact.name === contactName) {
+        // TODO: two contacts same name, manually select
+      }
+      if (contact.name === contactName) {
+        this.addContactToSelectedContacts(contact.id);
         isExistingContact = true;
-        break;
       }
     }
 
     if (!isExistingContact) {
-      this.selectedContacts.push(this.addEmailContact(contactName, true));
+      // TODO: no matching contacts
     }
   }
 
@@ -188,20 +184,39 @@ export class AlertConfigurationContactsComponent implements OnInit {
     }
   }
 
-  removeFromSelectedContacts(contact: any) {
-    const index = this.selectedContacts.indexOf(contact);
-    if (index >= 0) {
-      this.selectedContacts.splice(index, 1);
-    }
-    this.setContactAsSelected(contact, false);
+  // OPERATIONS By Id
+
+  isContactSelected(id: string ): boolean {
+    return this.selectedContacts.includes(id);
   }
 
-  setContactAsSelected(contact: any, isSelected) {
-    for (let _contact of this.exisitingContacts) {
-      if (_contact.name === contact.name && _contact.type === contact.type) {
-          _contact.isSelected = isSelected;
+  getContactFromId(id: string ): any {
+    for (let i = 0; i < this.exisitingContacts.length; i++) {
+      if (this.exisitingContacts[i].id === id) {
+        return this.exisitingContacts[i];
       }
     }
+  }
+
+  moreThanOneContactSelected(): boolean {
+    return (this.selectedContacts.length > 1);
+  }
+
+  removeContactFromSelectedContacts(id: string) {
+    for (let i = 0; i < this.selectedContacts.length; i++) {
+      if (this.selectedContacts[i] === id) {
+        this.selectedContacts.splice(i, 1);
+      }
+    }
+    this.contactRemovable = this.moreThanOneContactSelected();
+  }
+
+  addContactToSelectedContacts(id: string) {
+    if (!this.selectedContacts.includes(id)) {
+      this.selectedContacts.push(id);
+    }
+    console.log('**', this.moreThanOneContactSelected());
+    this.contactRemovable = this.moreThanOneContactSelected();
   }
 
 }
