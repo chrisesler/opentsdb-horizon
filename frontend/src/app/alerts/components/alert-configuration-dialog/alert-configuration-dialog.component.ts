cac53f1afc89e757d60f5ca29061f97bdca854d8
Chris Esler <cesler@oath.com>
2019-03-20 15:05:06 -0700

    more form stuff

--- frontend/src/app/alerts/components/alert-configuration-dialog/alert-configuration-dialog.component.ts
+++ frontend/src/app/alerts/components/alert-configuration-dialog/alert-configuration-dialog.component.ts
@@ -9,7 +9,10 @@ import {
     AfterViewInit
 } from '@angular/core';
 
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators, FormsModule, NgForm } from '@angular/forms';
+
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material';
 
 import {
     MatDialog,
@@ -67,12 +70,23 @@ export class AlertConfigurationDialogComponent implements OnInit, OnDestroy, Aft
 
     alertName: FormControl = new FormControl('');
 
+    // tslint:disable-next-line:no-inferrable-types
+    showThresholdAdvanced: boolean = false;
+
+    // FORM STUFF
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+    alertForm: FormGroup;
+    alertFormSub: Subscription;
+
+    // SUBSCRIPTIONS HOLDER
     subs: any = {};
 
     // tslint:disable-next-line:no-inferrable-types
     activeTabIndex: number = 0;
 
     constructor(
+        private fb: FormBuilder,
         public dialog: MatDialog,
         public dialogRef: MatDialogRef<AlertConfigurationDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public dialogData: any
@@ -81,6 +95,7 @@ export class AlertConfigurationDialogComponent implements OnInit, OnDestroy, Aft
         if (this.data.alertName) {
             this.alertName.setValue(this.data.alertName);
         }
+        this.setupForm();
     }
 
     ngOnInit() {
@@ -99,14 +114,122 @@ export class AlertConfigurationDialogComponent implements OnInit, OnDestroy, Aft
         // this.measureDetailForBalancer();
     }
 
-    /** Events */
-    /*configTabChange(index: any) {
-        console.log('CONFIG TAB CHANGE', index);
-        this.activeTabIndex = index;
-    }*/
+    setupForm() {
+        // TODO: need to check if there is something in this.data
+        this.alertForm = this.fb.group({
+            queries: this.fb.group({
+                groupingRules: this.fb.array([])
+            }),
+            threshold: this.fb.group({
+                type: 'simple',
+                query: '',
+                badStateValue: '',
+                warningStateValue: '',
+                recoveryNotification: 'minimum', // minimum or specific
+                recoveryStateValue: '', // if recoveryNotification is 'specific'
+                nagInterval: 'disabled',
+                missingData: 'doNotNotify',
+                slidingWindow: this.fb.group({
+                    direction: 'above',
+                    interval: 1,
+                    windowSize: '5 min'
+                })
+            }),
+            notification: this.fb.group({
+                notifyWhen: new FormControl([]),
+                recipients: this.fb.array([]),
+                subject: '',
+                message: '',
+                labels: this.fb.array([]),
+                runbookId: ''
+            })
+        });
 
-    changeThresholdType(thresholdType: string) {
-        this.selectedThresholdType = thresholdType;
+        this.alertFormSub = this.alertForm.valueChanges.subscribe( val => {
+            console.log('FORM CHANGE', val);
+        });
+
+    }
+
+    get thresholdControls() {
+        return this.alertForm['controls'].threshold['controls'];
+    }
+
+    get thresholdType() {
+        return this.thresholdControls.type.value;
+    }
+
+    get thresholdRecoveryNotification() {
+        return this.alertForm.get('threshold').get('recoveryNotification').value;
+    }
+
+    get groupRulesLabelValues() {
+        return this.alertForm['controls'].queries.get('groupingRules');
+    }
+
+    get notificationLabelValues() {
+        return this.alertForm['controls'].notification.get('labels');
+    }
+
+    get alertStateDirection() {
+        return this.alertForm.get('threshold').get('slidingWindow').get('direction').value;
+    }
+
+    get recoveryStateDirection() {
+        const valCheck = this.alertForm.get('threshold').get('slidingWindow').get('direction').value;
+        if (valCheck === 'above') {
+            return 'below';
+        }
+        return 'above';
+    }
+
+    /** Events */
+
+    removeNotificationLabelValue(i: number) {
+        const control = <FormArray>this.notificationLabelValues;
+        control.removeAt(i);
+    }
+
+    addNotificationLabelValue(event: MatChipInputEvent) {
+        const input = event.input;
+        const value = event.value;
+
+        // Add our fruit
+        if ((value || '').trim()) {
+            const control = <FormArray>this.notificationLabelValues;
+            control.push(new FormControl(value.trim()));
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    removeQueryGroupRuleValue(i: number) {
+        const control = <FormArray>this.groupRulesLabelValues;
+        control.removeAt(i);
+    }
+
+    addQueryGroupRuleValue(event: MatChipInputEvent) {
+        const input = event.input;
+        const value = event.value;
+
+        // Add our fruit
+        if ((value || '').trim()) {
+            const control = <FormArray>this.groupRulesLabelValues;
+            control.push(new FormControl(value.trim()));
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    recoveryNotificationChange(event: any) {
+        const control = <FormControl>this.alertForm.get('threshold')['controls'].recoveryNotification;
+        control.setValue(event.value);
     }
 
     /** Privates */
@@ -115,21 +238,10 @@ export class AlertConfigurationDialogComponent implements OnInit, OnDestroy, Aft
         const dialogConf: MatDialogConfig = new MatDialogConfig();
         dialogConf.autoFocus = false;
         dialogConf.width = '300px';
-        // dialogConf.maxWidth = '600px';
-        // dialogConf.height = 'auto';
-        // dialogConf.hasBackdrop = true;
-        // dialogConf.direction = 'ltr';
-        // dialogConf.backdropClass = 'snooze-alert-dialog-backdrop';
         dialogConf.panelClass = 'name-alert-dialog-panel';
-        /*dialogConf.position = <DialogPosition>{
-            top: '48px',
-            bottom: '0px',
-            left: '0px',
-            right: '0px'
-        };*/
 
         this.nameAlertDialog = this.dialog.open(NameAlertDialogComponent, dialogConf);
-        // this.snoozeAlertDialog.updatePosition({ top: '48px' });
+
         this.nameAlertDialog.afterClosed().subscribe((dialog_out: any) => {
             console.log('NAME ALERT DIALOG [afterClosed]', dialog_out);
             if (dialog_out && dialog_out.alertName) {
