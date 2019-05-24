0a8d8b979a713eb364c902f7b0bc1934ab521d99
Chris Larsen <clarsen@verizonmedia.com>
2019-05-24 15:23:42 -0700

    Fix the counter flags sent to TSDB to reset negative rates properly.

--- frontend/src/app/core/services/yamas.service.ts
+++ frontend/src/app/core/services/yamas.service.ts
@@ -179,6 +179,7 @@ export class YamasService {
                             'type': 'rate',
                             'interval': funs[i].val,
                             'counter': fxCall === 'RateOfChange' ? false : true,
+                            'dropResets': fxCall === 'RateOfChange' ? false : true,
                             'sources': [ds]
                         };
                     queries.push(q);
