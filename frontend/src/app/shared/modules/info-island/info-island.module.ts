1eb5fe8c701ec139393fa238f302aab698fe4fbc
Chris Esler <cesler@oath.com>
2019-07-15 17:47:01 -0700

    adding island test component to module

--- frontend/src/app/shared/modules/info-island/info-island.module.ts
+++ frontend/src/app/shared/modules/info-island/info-island.module.ts
@@ -6,6 +6,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { InfoIslandService } from './services/info-island.service';
 import { InfoIslandComponent } from './containers/info-island.component';
+import { IslandTestComponent } from './components/island-test/island-test.component';
+
+import { ISLAND_DATA } from './info-island.tokens';
 
 @NgModule({
     imports: [
@@ -15,7 +18,13 @@ import { InfoIslandComponent } from './containers/info-island.component';
         MaterialModule
     ],
     providers: [ InfoIslandService ],
-    declarations: [ InfoIslandComponent ],
-    entryComponents: [ InfoIslandComponent ]
+    declarations: [
+        InfoIslandComponent,
+        IslandTestComponent
+    ],
+    entryComponents: [
+        InfoIslandComponent,
+        IslandTestComponent
+    ]
 })
 export class InfoIslandModule { }
