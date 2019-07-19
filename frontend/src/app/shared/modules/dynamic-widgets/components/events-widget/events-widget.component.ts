d1b67e1c9c42858d7bdf785cdabed942fe2eb0c5
Chris Esler <cesler@oath.com>
2019-07-19 16:38:53 -0700

    cleanup and adding missing style class

--- frontend/src/app/shared/modules/dynamic-widgets/components/events-widget/events-widget.component.ts
+++ frontend/src/app/shared/modules/dynamic-widgets/components/events-widget/events-widget.component.ts
@@ -1,133 +1,136 @@
-import { Component, OnInit , HostBinding, Input, OnDestroy, OnChanges, SimpleChanges, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, HostBinding, Input, OnDestroy, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 import { IntercomService, IMessage } from '../../../../../core/services/intercom.service';
-import { Subscription, Observable } from 'rxjs';
-import { Store, Select } from '@ngxs/store';
+import { Subscription } from 'rxjs';
+
 import { UtilsService } from '../../../../../core/services/utils.service';
 import { DateUtilsService } from '../../../../../core/services/dateutils.service';
 
 @Component({
-  selector: 'app-events-widget',
-  templateUrl: './events-widget.component.html',
-  styleUrls: ['./events-widget.component.scss']
+    // tslint:disable-next-line: component-selector
+    selector: 'events-widget',
+    templateUrl: './events-widget.component.html',
+    styleUrls: []
 })
 export class EventsWidgetComponent implements OnInit, OnDestroy, OnChanges {
-  @HostBinding('class.events-widget') private _componentClass = true;
+    @HostBinding('class.widget-panel-content') private _hostClass = true;
+    @HostBinding('class.events-widget') private _componentClass = true;
 
-  constructor(  private interCom: IntercomService,
-                private store: Store,
-                private util: UtilsService,
-                private dateUtil: DateUtilsService,
-                private cdRef: ChangeDetectorRef) {  }
+    constructor(
+        private interCom: IntercomService,
+        private util: UtilsService,
+        private dateUtil: DateUtilsService,
+        private cdRef: ChangeDetectorRef
+    ) { }
 
-  /** Inputs */
-  @Input() editMode: boolean;
-  @Input() widget: any; // includes query
+    /** Inputs */
+    @Input() editMode: boolean;
+    @Input() widget: any; // includes query
 
-  /** Local Variables */
-  events: any[];
-  startTime: number;
-  endTime: number;
-  timezone: string;
+    /** Local Variables */
+    events: any[];
+    startTime: number;
+    endTime: number;
+    timezone: string;
 
-  // state control
-  isDataRefreshRequired = false;
-  private listenSub: Subscription;
+    // state control
+    isDataRefreshRequired = false;
+    private listenSub: Subscription;
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.widget = {... this.util.setDefaultEventsConfig(this.widget, true)};
-    this.getEvents();
+        this.widget = { ... this.util.setDefaultEventsConfig(this.widget, true) };
+        this.getEvents();
 
-    this.listenSub = this.interCom.responseGet().subscribe((message: IMessage) => {
-      // console.log(message);
+        this.listenSub = this.interCom.responseGet().subscribe((message: IMessage) => {
+            // console.log(message);
 
-      switch (message.action) {
-        case 'TimeChanged':
-          this.getEvents();
-          break;
-        case 'reQueryData':
-          this.getEvents();
-          break;
-      }
+            switch (message.action) {
+                case 'TimeChanged':
+                    this.getEvents();
+                    break;
+                case 'reQueryData':
+                    this.getEvents();
+                    break;
+            }
 
-      if (message && (message.id === this.widget.id)) {
-        switch (message.action) {
-          case 'getUpdatedWidgetConfig': // called when switching to presentation view
-            this.widget = message.payload.widget;
-            break;
-          case 'updatedEvents':
-            this.events = message.payload.events;
-            this.timezone = message.payload.time.zone;
-            this.startTime = this.dateUtil.timeToMoment(message.payload.time.start, this.timezone).unix() * 1000;
-            this.endTime = this.dateUtil.timeToMoment(message.payload.time.end, this.timezone).unix() * 1000;
-            this.cdRef.detectChanges();
-            break;
+            if (message && (message.id === this.widget.id)) {
+                switch (message.action) {
+                    case 'getUpdatedWidgetConfig': // called when switching to presentation view
+                        this.widget = message.payload.widget;
+                        break;
+                    case 'updatedEvents':
+                        this.events = message.payload.events;
+                        this.timezone = message.payload.time.zone;
+                        this.startTime = this.dateUtil.timeToMoment(message.payload.time.start, this.timezone).unix() * 1000;
+                        this.endTime = this.dateUtil.timeToMoment(message.payload.time.end, this.timezone).unix() * 1000;
+                        this.cdRef.detectChanges();
+                        break;
+                }
+            }
+        });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes) {
+            // console.log(changes);
         }
-      }
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes) {
-      // console.log(changes);
     }
-  }
 
-  getEvents() {
-      this.interCom.requestSend({
-        id: this.widget.id,
-        action: 'getEventData',
-        payload: this.widget
-    });
-  }
-
-  applyConfig() {
-    const cloneWidget = { ...this.widget };
-    cloneWidget.id = cloneWidget.id.replace('__EDIT__', '');
-    this.interCom.requestSend({
-        action: 'updateWidgetConfig',
-        id: cloneWidget.id,
-        payload: { widget: cloneWidget, isDataRefreshRequired: this.isDataRefreshRequired }
-    });
-    this.closeViewEditMode();
-  }
-
-  closeViewEditMode() {
-    this.interCom.requestSend({
-        action: 'closeViewEditMode',
-        payload: 'dashboard'
-    });
-  }
-
-  ngOnDestroy() {
-    this.listenSub.unsubscribe();
-  }
-
-  updateConfig(message) {
-    switch ( message.action ) {
-      case 'SetEventQuerySearch':
-        this.setEventQuerySearch(message.payload.search);
-        break;
-      case 'SetEventQueryNamespace':
-        this.setEventQueryNamespace(message.payload.namespace);
-        break;
+    getEvents() {
+        this.interCom.requestSend({
+            id: this.widget.id,
+            action: 'getEventData',
+            payload: this.widget
+        });
     }
-  }
 
-  setEventQuerySearch(search: string) {
-    // todo: set correctly
-    const deepClone = JSON.parse(JSON.stringify(this.widget));
-    deepClone.eventQueries[0].search = search;
-    this.widget.eventQueries = {... deepClone.eventQueries};
-    this.getEvents();
-  }
+    applyConfig() {
+        const cloneWidget = { ...this.widget };
+        cloneWidget.id = cloneWidget.id.replace('__EDIT__', '');
+        this.interCom.requestSend({
+            action: 'updateWidgetConfig',
+            id: cloneWidget.id,
+            payload: { widget: cloneWidget, isDataRefreshRequired: this.isDataRefreshRequired }
+        });
+        this.closeViewEditMode();
+    }
 
-  setEventQueryNamespace(namespace: string) {
-    // todo: set correctly
-    const deepClone = JSON.parse(JSON.stringify(this.widget));
-    deepClone.eventQueries[0].namespace = namespace;
-    this.widget.eventQueries = {... deepClone.eventQueries};
-    this.getEvents();
-  }
+    closeViewEditMode() {
+        this.interCom.requestSend({
+            action: 'closeViewEditMode',
+            payload: 'dashboard'
+        });
+    }
+
+    ngOnDestroy() {
+        this.listenSub.unsubscribe();
+    }
+
+    updateConfig(message) {
+        switch (message.action) {
+            case 'SetEventQuerySearch':
+                this.setEventQuerySearch(message.payload.search);
+                break;
+            case 'SetEventQueryNamespace':
+                this.setEventQueryNamespace(message.payload.namespace);
+                break;
+        }
+    }
+
+    setEventQuerySearch(search: string) {
+        // todo: set correctly
+        const deepClone = JSON.parse(JSON.stringify(this.widget));
+        deepClone.eventQueries[0].search = search;
+        this.widget.eventQueries = { ...deepClone.eventQueries };
+        this.getEvents();
+    }
+
+    setEventQueryNamespace(namespace: string) {
+        // todo: set correctly
+        const deepClone = JSON.parse(JSON.stringify(this.widget));
+        deepClone.eventQueries[0].namespace = namespace;
+        this.widget.eventQueries = { ...deepClone.eventQueries };
+        this.getEvents();
+    }
 
 }
