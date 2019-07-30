f3ddb04d0c02853452d3766565114e7524d91796
Hill Nguyen <hilln@oath.com>
2019-07-30 11:15:45 -0700

    get groupByTag

--- frontend/src/app/core/services/multigraph.service.ts
+++ frontend/src/app/core/services/multigraph.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class MultigraphService {
 
   constructor() { }
+
+
+  // to get all groupby tags of a wigdet
+  getGroupByTags(queries: any[]): string[] {
+    let ret = [];
+    for (let i = 0; i < queries.length; i++) {
+      const query = queries[i];
+      for (let j = 0; j < query.metrics.length; j++) {
+        const metric = query.metrics[j];
+        if (metric.groupByTags && metric.groupByTags.length) {
+          ret = ret.concat(metric.groupByTags.filter((item) => {
+            return ret.indexOf(item) < 0;
+          }));
+        }
+      }
+    }
+    return ret;
+  }
 }
