b8f403170d6aed30cdec13cd2dde4f90557d5f84
Zack Burgess <zb@yahoo-inc.com>
2019-11-08 11:12:50 -0800

    validate threshold values

--- frontend/src/app/alerts/components/alert-details/children/alert-details-metric-period-over-period/alert-details-metric-period-over-period.component.ts
+++ frontend/src/app/alerts/components/alert-details/children/alert-details-metric-period-over-period/alert-details-metric-period-over-period.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UtilsService } from '../../../../../core/services/utils.service';
+import { FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
+// tslint:disable:max-line-length
 @Component({
   selector: 'alert-details-metric-period-over-period',
   templateUrl: './alert-details-metric-period-over-period.component.html',
@@ -23,6 +25,32 @@ export class AlertDetailsMetricPeriodOverPeriodComponent implements OnInit {
   slidingWindowPresets = [60, 300, 600, 900, 3600, 3600 * 6, 3600 * 24];
   periodPresets = [3600, 3600 * 24, 3600 * 24 * 7];
   maxSlidingWindow = 3600 * 24; // 1 day
+
+  thresholdChanged = false;
+
+  // form control
+  lookbacks = new FormControl('');
+  badUpperThreshold = new FormControl('');
+  warnUpperThreshold = new FormControl('');
+  badLowerThreshold = new FormControl('');
+  warnLowerThreshold = new FormControl('');
+
+  get anyErrors(): boolean {
+    if (this.lookbacks.errors) {
+        return true;
+    } else if (this.badUpperThreshold.errors) {
+      return true;
+    } else if (this.warnUpperThreshold.errors) {
+      return true;
+    } else if (this.badLowerThreshold.errors) {
+      return true;
+    } else if (this.warnLowerThreshold.errors) {
+      return true;
+    } else if (!this.atleastOneThresholdSet()) {
+      return true;
+    }
+    return false;
+}
 
   ngOnInit() {
     if (this.viewMode) {
@@ -66,17 +94,55 @@ export class AlertDetailsMetricPeriodOverPeriodComponent implements OnInit {
   }
 
   updateConfig(prop, val) {
-    let thresholdChanged = false;
+    if (prop === 'badUpperThreshold' || prop === 'warnUpperThreshold' || prop === 'badLowerThreshold' || prop === 'warnLowerThreshold') {
+      this.thresholdChanged = true;
+      // this.configIsValid.emit(true);
+    }
     if (prop === 'delayEvaluation') {
       this.config[prop] = val;
     } else {
       this.config.singleMetric[prop] = val;
     }
-    if (prop === 'badUpperThreshold' || prop === 'warnUpperThreshold' || prop === 'badLowerThreshold' || prop === 'warnLowerThreshold') {
-      thresholdChanged = true;
-      this.configIsValid.emit(true);
+
+    this.updateValidators();
+
+    if (!this.anyErrors) {
+      this.configChange.emit({ thresholdChanged: this.thresholdChanged, config: {...this.config}});
+      this.thresholdChanged = false;
     }
-    this.configChange.emit({thresholdChanged, config: {...this.config}});
+  }
+
+  updateValidators() {
+    this.lookbacks = new FormControl(this.config.singleMetric['lookbacks'], [Validators.max(10), Validators.min(1)]);
+    this.badUpperThreshold = new FormControl(this.config.singleMetric['badUpperThreshold'],   [Validators.min(0), this.thresholdValidator('warnUpperThreshold')]);
+    this.warnUpperThreshold = new FormControl(this.config.singleMetric['warnUpperThreshold'], [Validators.min(0), this.thresholdValidator('badUpperThreshold')]);
+    this.badLowerThreshold = new FormControl(this.config.singleMetric['badLowerThreshold'],   [Validators.min(0), this.thresholdValidator('warnLowerThreshold')]);
+    this.warnLowerThreshold = new FormControl(this.config.singleMetric['warnLowerThreshold'], [Validators.min(0), this.thresholdValidator('badLowerThreshold')]);
+  }
+
+  thresholdValidator(thresholdToCompare: string): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        let forbidden;
+        if (control.value === '') {
+          forbidden = false;
+        } else if (thresholdToCompare.includes('bad')) { // compare warn value to bad threshold
+          forbidden = this.isValueLargerThanThreshold(thresholdToCompare, control.value);
+        } else { // compare bad value to warn threshold
+          forbidden = !this.isValueLargerThanThreshold(thresholdToCompare, control.value);
+        }
+        return forbidden ? { 'forbiddenValue': { value: control.value } } : null;
+    };
+  }
+
+  isValueLargerThanThreshold(threshold: string, value: string): boolean {
+    return Number(value) > Number(this.config.singleMetric[threshold]);
+  }
+
+  atleastOneThresholdSet() {
+    return this.config.singleMetric['badUpperThreshold'] ||
+           this.config.singleMetric['warnUpperThreshold'] ||
+           this.config.singleMetric['warnLowerThreshold'] ||
+           this.config.singleMetric['badLowerThreshold'];
   }
 
 }
