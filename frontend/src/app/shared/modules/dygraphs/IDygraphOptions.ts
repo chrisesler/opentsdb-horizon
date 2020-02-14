dc179e583981062fc698a62473feb761831d03bd
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2020-02-14 10:11:10 -0800

    linechart: increasing no. of y-axis labels when the chart height is small

--- frontend/src/app/shared/modules/dygraphs/IDygraphOptions.ts
+++ frontend/src/app/shared/modules/dygraphs/IDygraphOptions.ts
@@ -16,6 +16,7 @@ export interface DygraphOptionsAxis {
     independentTicks?: true;
     digitsAfterDecimal?: number;
     axisLabelWidth?: number;
+    pixelsPerLabel?: number;
 }
 
 export interface IDygraphOptions {
