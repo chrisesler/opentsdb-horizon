71846845c7e1783699dd6763a65a00a711b739f9
Syed Abuthahir Sait Seenipeer <syed@oath.com>
2020-02-18 11:25:56 -0800

    removing unnecessary modulues

--- frontend/src/app/core/services/dashboard-converter.service.ts
+++ frontend/src/app/core/services/dashboard-converter.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UtilsService } from './utils.service';
 import { HttpService } from '../http/http.service';
-import { of, Observable, forkJoin } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
-import { ObserversModule } from '@angular/cdk/observers';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
